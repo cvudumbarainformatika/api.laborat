692a16389037dbf96c29ee22a3ee644c0ab5215a
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2022-09-24 14:31:45 +0700

    aish

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/autogen', [AutogenController::class, 'index']);
-<<<<<<< HEAD
 Route::get('/autogen/coba', [AutogenController::class, 'getDetOrderList']);
-=======
-Route::get('/autogen/coba', [AutogenController::class, 'coba']);
-
-Route::get('/scrapper/coba', [ScrapperController::class, 'index']);
->>>>>>> 5a5865f34e9a0866bee773204e2d5343edd57e01
