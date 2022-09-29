6c5665a8a663e45624648f51cb67db7c0ddfbf09
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2022-09-29 15:56:56 +0700

    kantor

--- app/Http/Controllers/Api/penunjang/PemeriksaanLaboratController.php
+++ app/Http/Controllers/Api/penunjang/PemeriksaanLaboratController.php
@@ -11,9 +11,21 @@ class PemeriksaanLaboratController extends Controller
 {
     public function groupped()
     {
-        $query = collect(PemeriksaanLaborat::all());
-        $data= $query->groupBy('rs21');
+        // $query = collect(PemeriksaanLaborat::all());
+        // $data= $query->groupBy('rs21');
 
+        $data = PemeriksaanLaborat::query()
+        ->where('hidden','=','')
+            ->when(request('q'), function($search, $q){
+                if (request('p') === 'non') {
+                    $search->where('rs21','=','')
+                    ->where('rs2', 'LIKE', '%' . $q . '%');
+                } else {
+                    $search->where('rs21','<>','')
+                    ->where('rs2', 'LIKE', '%' . $q . '%');
+                }
+
+        })->limit(20)->get();
         return new JsonResponse($data);
     }
 }
