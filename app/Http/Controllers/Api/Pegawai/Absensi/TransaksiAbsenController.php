42cdbb59fc662f7de5dd33171a0b663957a94a98
duro06 <andi.meka.025@gmail.com>
2022-11-22 15:57:30 +0700

    22 11 2022 15:57

--- app/Http/Controllers/Api/Pegawai/Absensi/TransaksiAbsenController.php
+++ app/Http/Controllers/Api/Pegawai/Absensi/TransaksiAbsenController.php
@@ -34,14 +34,39 @@ class TransaksiAbsenController extends Controller
             ->whereDate('tanggal', '>=', $thisYear . '-' . $month . '-01')
             ->whereDate('tanggal', '<=', $thisYear . '-' . $month . '-31')
             ->orderBy(request('order_by'), request('sort'))
+            ->with('kategory')
             ->get();
-        $collects = collect($data);
         $tanggals = [];
         foreach ($data as $key) {
             $temp = date('Y/m/d', strtotime($key['tanggal']));
+            $week = date('W', strtotime($key['tanggal']));
+            $toIn = explode(':', $key['kategory']->masuk);
+            $act = explode(':', $key['masuk']);
+            $jam = (int)$act[0] - (int)$toIn[0];
+            $menit =  (int)$act[1] - (int)$toIn[1];
+            $detik =  (int)$act[2] - (int)$toIn[2];
+
+            // $key['jam'] = $jam;
+            // $key['menit'] = $menit;
+            // $key['detik'] = $detik;
+            if ($jam > 0 || $menit > 10) {
+                $key['terlambat'] = 'yes';
+            } else {
+                $key['terlambat'] = 'no';
+            }
+            $dMenit = $menit >= 10 ? $menit : '0' . $menit;
+            $dDetik = $detik >= 10 ? $detik : '0' . $detik;
+            $diff = $jam . ':' . $dMenit . ':' . $dDetik;
+            $key['diff'] = $diff;
+            $key['week'] = $week;
             array_push($tanggals, $temp);
         };
+        $collects = collect($data);
+        $grouped = $collects->groupBy('week');
+        $telat = $collects->where('terlambat', 'yes')->count();
         return new JsonResponse([
+            'telat' => $telat,
+            'weeks' => $grouped,
             'tanggals' => $tanggals,
             'data' => $data,
         ], 200);
