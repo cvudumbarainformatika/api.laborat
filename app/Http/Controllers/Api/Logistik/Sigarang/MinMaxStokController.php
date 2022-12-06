be150a6677ef2a040eb45ce18f1c5698584dc0f0
duro06 <andi.meka.025@gmail.com>
2022-12-07 05:51:24 +0700

    pagi

---
+++ app/Http/Controllers/Api/Logistik/Sigarang/MinMaxStokController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\Logistik\Sigarang;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MinMaxStokController extends Controller
+{
+    //
+}
