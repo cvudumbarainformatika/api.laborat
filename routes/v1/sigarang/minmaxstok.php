bfa3132885138da7908ae446d90b1e72e0aeca78
duro06 <andi.meka.025@gmail.com>
2022-12-07 14:09:28 +0700

    persiapan route dan controller stok

--- routes/v1/sigarang/minmaxstok.php
+++ routes/v1/sigarang/minmaxstok.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\Logistik\Sigarang\MinMaxStokController;
+use App\Http\Controllers\Api\Logistik\Sigarang\MinMaxStokDepoController;
+use App\Http\Controllers\Api\Logistik\Sigarang\MinMaxStokPenggunaController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -13,4 +15,24 @@ Route::group([
     Route::get('/minmaxstok', [MinMaxStokController::class, 'minmaxstok']);
     Route::post('/store', [MinMaxStokController::class, 'store']);
     Route::post('/destroy', [MinMaxStokController::class, 'destroy']);
+});
+
+Route::group([
+    'middleware' => 'auth:api',
+    // 'middleware' => 'jwt.verify',
+    'prefix' => 'minmaxdepostok'
+], function () {
+    Route::get('/index', [MinMaxStokDepoController::class, 'index']);
+    Route::post('/store', [MinMaxStokDepoController::class, 'store']);
+    Route::post('/destroy', [MinMaxStokDepoController::class, 'destroy']);
+});
+
+Route::group([
+    'middleware' => 'auth:api',
+    // 'middleware' => 'jwt.verify',
+    'prefix' => 'minmaxpenggunastok'
+], function () {
+    Route::get('/index', [MinMaxStokPenggunaController::class, 'index']);
+    Route::post('/store', [MinMaxStokPenggunaController::class, 'store']);
+    Route::post('/destroy', [MinMaxStokPenggunaController::class, 'destroy']);
 });
