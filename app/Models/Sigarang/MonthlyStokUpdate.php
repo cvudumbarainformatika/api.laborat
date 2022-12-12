8f5f7c34f09c6a771c825b4fbe21c27a641ec7f4
duro06 <andi.meka.025@gmail.com>
2022-12-12 11:05:13 +0700

    persiapan record stok

---
+++ app/Models/Sigarang/MonthlyStokUpdate.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models\Sigarang;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MonthlyStokUpdate extends Model
+{
+    use HasFactory;
+    protected $connection = 'sigarang';
+    protected $guarded = ['id'];
+
+    public function barang()
+    {
+        return $this->belongsTo(BarangRS::class, 'kode_rs', 'kode');
+    }
+    public function depo()
+    {
+        return $this->belongsTo(Gudang::class, 'kode_ruang', 'kode');
+    }
+    public function ruang()
+    {
+        return $this->belongsTo(Ruang::class, 'kode_ruang', 'kode');
+    }
+    public function pengguna()
+    {
+        return $this->belongsTo(Pengguna::class, 'kode_ruang', 'kode');
+    }
+}
