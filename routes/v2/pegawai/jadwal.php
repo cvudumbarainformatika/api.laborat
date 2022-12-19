a95875139aa1957be47f07988cca54d1450b4a95
duro06 <andi.meka.025@gmail.com>
2022-12-19 14:31:06 +0700

    ubah

--- routes/v2/pegawai/jadwal.php
+++ routes/v2/pegawai/jadwal.php
@@ -19,3 +19,18 @@ Route::group([
     Route::post('/update', [JadwalController::class, 'update']);
     Route::post('/sync', [JadwalController::class, 'sycncroneJadwal']);
 });
+
+Route::group([
+    // 'middleware' => 'auth:api',
+    // 'middleware' => 'jwt.verify',
+    'prefix' => 'absensi/jadwal/perubahan'
+], function () {
+    // Route::get('/kategori', [JadwalController::class, 'getKategories']);
+    // Route::get('/hari', [JadwalController::class, 'getDays']);
+    // Route::get('/by-user', [JadwalController::class, 'getByUser']);
+    // Route::get('/rekap-by-user', [TransaksiAbsenController::class, 'getRekapByUser']);
+    // Route::get('/absen-today', [TransaksiAbsenController::class, 'getAbsenToday']);
+    // Route::post('/simpan', [JadwalController::class, 'create']);
+    // Route::post('/update', [JadwalController::class, 'update']);
+    Route::post('/sync', [JadwalController::class, 'sycncroneJadwal']);
+});
