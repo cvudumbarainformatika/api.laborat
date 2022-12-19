83d278e2d181affd586d60c8c1227e8ba7e11dd2
duro06 <andi.meka.025@gmail.com>
2022-12-19 22:42:05 +0700

    new password end point

--- routes/v2/auth.php
+++ routes/v2/auth.php
@@ -14,6 +14,7 @@ Route::group([
     'middleware' => 'jwt.verify',
     'prefix' => 'user'
 ], function () {
+    Route::post('/reset-device', [AuthController::class, 'resetDevice']);
     Route::get('/me', [AuthController::class, 'me']);
-    Route::get('/logout', [AuthController::class, 'logout']);
+    Route::get('/new-password', [AuthController::class, 'newPassword']);
 });
