2953db9112eb12628fff6a5c3d3d07923d4b66bc
duro06 <andi.meka.025@gmail.com>
2022-12-27 15:41:54 +0700

    ad

--- app/Http/Controllers/AutogenController.php
+++ app/Http/Controllers/AutogenController.php
@@ -514,16 +514,17 @@ class AutogenController extends Controller
             ->get();
 
 
-        $col = collect($data);
+        $data['masuk'] = $data;
         $libur = Libur::where('user_id', $user->id)
             ->whereDate('tanggal', '>=', $thisYear . '-' . $month . '-01')
             ->whereDate('tanggal', '<=', $thisYear . '-' . $month . '-31')
             ->latest()
             ->get();
 
-        $col['libur'] = $libur;
+        $data['libur'] = $libur;
+        // array_push($col, ['libur' => $libur]);
         // return new JsonResponse($data);
-        return new JsonResponse($col);
+        return new JsonResponse($data);
     }
 
     public function wawanpost(Request $request)
