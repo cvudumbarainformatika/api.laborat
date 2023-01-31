f440b68eab7b89a15cb6a9a7b10d73e11221f5ad
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-02-01 02:20:37 +0700

    sh

--- app/Http/Controllers/Api/Pegawai/Absensi/TransaksiAbsenController.php
+++ app/Http/Controllers/Api/Pegawai/Absensi/TransaksiAbsenController.php
@@ -445,11 +445,11 @@ class TransaksiAbsenController extends Controller
             // } else if (request('flag') === 'all' && request('ruang') === 'all') {
             //     return $query->where('aktif', '=', 'AKTIF');
             // }
-            return $query->where('ruang', '!=', 'all')
-                ->where('flag', '!=', 'all')
-                ->where('flag', '=', request('flag'))
-                ->where('ruang', '=', request('ruang'))
-                ->where('aktif', '=', 'AKTIF');
+            return $query->where('aktif', '=', 'AKTIF')
+                ->where('ruang', '<>', 'all')
+                ->orWhere('ruang', '=', request('ruang'))
+                ->where('flag', '<>', 'all')
+                ->orWhere('flag', '=', request('flag'));
         })
             ->with(["transaksi_absen.kategory", "jenis_pegawai", "relasi_jabatan", "ruangan", "transaksi_absen" => function ($q) use ($periode) {
                 $split = explode("-", $periode);
