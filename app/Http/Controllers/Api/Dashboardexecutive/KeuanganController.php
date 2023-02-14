d5e72405e0a91816f9d9aa30519c0f6baae389f2
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-02-14 11:18:59 +0700

    coba

--- app/Http/Controllers/Api/Dashboardexecutive/KeuanganController.php
+++ app/Http/Controllers/Api/Dashboardexecutive/KeuanganController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Dashboardexecutive;
 
 use App\Http\Controllers\Controller;
 use App\Models\Agama;
+use App\Models\Executive\HeaderPenerimaan;
 use App\Models\Executive\KeuTransPendapatan;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -12,9 +13,16 @@ class KeuanganController extends Controller
 {
     public function pendapatan()
     {
-        $data = KeuTransPendapatan::where('noTrans', 'not like', "%TBP-UJ%")
-            ->whereMonth('tgl', request('month'))
-            ->whereYear('tgl', request('year'))->get();
+        // $data = KeuTransPendapatan::where('noTrans', 'not like', "%TBP-UJ%")
+        //     ->whereMonth('tgl', request('month'))
+        //     ->whereYear('tgl', request('year'))->get();
+        $data = HeaderPenerimaan::where('noTrans', 'not like', "%TBP-UJ%")
+            ->whereMonth('rs2', request('month'))
+            ->whereYear('rs2', request('year'))
+            ->where('setor', '=', 'Setor')
+            ->whereNull('tglBatal')
+            ->orWhere('tglBatal', '=', '0000-00-00 00:00:00')
+            ->get();
         return response()->json($data);
     }
 }
