977ab72e1c4a9307feabd68285a9278455a59204
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-02-16 09:32:34 +0700

    assss

--- app/Http/Controllers/Api/Dashboardexecutive/KepegawaianController.php
+++ app/Http/Controllers/Api/Dashboardexecutive/KepegawaianController.php
@@ -11,15 +11,22 @@ class KepegawaianController extends Controller
 {
     public function index()
     {
-        $struktural = DB::connection('kepex')->select(
+        $kategori_pegawai = DB::connection('kepex')->select(
             "select p.kategoripegawai kode_kat,namakategoripeg,count(nip) jumlah
 		            from pegawai p left join m_kategori_peg k on p.kategoripegawai=k.kodekategoripeg
                     where aktif='AKTIF' and jenispegawai!='Struktural' group by kategoripegawai order by k.kodekategoripeg"
         );
 
-        $data = array(
-            'struktural' => $struktural,
+        $status_pegawai = DB::connection('kepex')->select(
+            "select flag status,jp.jenispegawai,(select count(nip) from pegawai where flag=status and kelamin='Laki-Laki' and aktif='AKTIF') l,
+            (Select count(nip) from pegawai where flag=status and kelamin='Perempuan' and aktif='AKTIF') p,
+            count(nip) jumlah from pegawai p,m_jenispegawai jp where p.flag=jp.kode_jenis and aktif='AKTIF' group by flag"
         );
-        return response()->json($struktural);
+
+        $data = array(
+            'kategori_pegawai' => $kategori_pegawai,
+            'status_pegawai' => $status_pegawai,
+        );
+        return response()->json($status_pegawai);
     }
 }
