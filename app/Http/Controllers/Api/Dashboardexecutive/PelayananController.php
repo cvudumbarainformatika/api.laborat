48c2b2c6b933532ecbe8661755941b615998e758
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-02-20 18:18:10 +0700

    fdfsd

--- app/Http/Controllers/Api/Dashboardexecutive/PelayananController.php
+++ app/Http/Controllers/Api/Dashboardexecutive/PelayananController.php
@@ -84,9 +84,9 @@ class PelayananController extends Controller
             ->whereNotIn('rs8', ['POL014', 'POL005', 'POL025'])
             ->whereDate('rs3', Carbon::today())
             ->where('rs19', '=', '')
-            ->join('rs15', 'rs17.rs2', '=', 'rs15.rs1') // JOIN DATA PASIEN
-            ->join('rs19', 'rs17.rs8', '=', 'rs19.rs1') // JOIN DATA MASTER POLI
-            ->join('rs9', 'rs17.rs14', '=', 'rs9.rs1') // JOIN DATA MASTER CARA BAYAR
+            // ->join('rs15', 'rs17.rs2', '=', 'rs15.rs1') // JOIN DATA PASIEN
+            // ->join('rs19', 'rs17.rs8', '=', 'rs19.rs1') // JOIN DATA MASTER POLI
+            // ->join('rs9', 'rs17.rs14', '=', 'rs9.rs1') // JOIN DATA MASTER CARA BAYAR
             ->get();
 
         $poli_hariinisudah = DB::select(
