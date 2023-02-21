ee09f7b716b98206b4bf66d20e1380ad68d39fa2
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-02-21 22:38:32 +0700

    af

--- app/Http/Controllers/Api/Dashboardexecutive/PelayananController.php
+++ app/Http/Controllers/Api/Dashboardexecutive/PelayananController.php
@@ -122,7 +122,8 @@ class PelayananController extends Controller
             ->join('rs141', 'rs17.rs1', '=', 'rs141.rs1')
             ->select('rs17.rs1', 'rs17.rs3', 'rs17.rs2', 'rs17.rs8', 'rs17.rs14', 'rs17.rs19')
             ->whereNotIn('rs17.rs8', ['POL014', 'POL005', 'POL025'])
-            ->whereDate('rs17.rs3', '=', Carbon::today())
+            // ->whereDate('rs17.rs3', '=', Carbon::today())
+            ->where(DB::raw("DATE(rs17.rs3) = '" . date('Y-m-d', $time) . "'"))
             ->where('rs17.rs19', '=', '1')
             ->get();
 
