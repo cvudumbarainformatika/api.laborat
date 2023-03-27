c5f0f4e309ecaf059af8bffca8febb9c5f404b6d
kentangkotak <ariezlnd69@gmail.com>
2023-03-27 13:01:17 +0700

    wewewewew

---
+++ app/Models/Msistembayar.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Msistembayar extends Model
+{
+    use HasFactory;
+}
