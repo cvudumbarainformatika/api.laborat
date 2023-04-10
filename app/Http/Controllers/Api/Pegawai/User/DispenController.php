6d3159d390b20ff843860d9c8fd7050ca0013127
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-04-10 21:56:27 +0700

    ss

---
+++ app/Http/Controllers/Api/Pegawai/User/DispenController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api\Pegawai\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\Pegawai\Akses\User;
+use App\Models\Pegawai\Alpha;
+use App\Models\Pegawai\JadwalAbsen;
+use App\Models\Pegawai\Libur;
+use App\Models\Pegawai\TransaksiAbsen;
+use App\Models\Sigarang\Pegawai;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class DispenController extends Controller
+{
+    //
+    public function index()
+    {
+        $data = Pegawai::where('aktif', '=', 'AKTIF')
+            ->when(request('q') ?? false, function ($search, $q) {
+                $search->where('nama', 'LIKE', '%' . $q . '%');
+            })->with(['ruangan'])
+            ->paginate(request('per_page'));
+
+        return new JsonResponse($data);
+    }
+}
--- routes/v1/pegawai/libur.php
+++ routes/v1/pegawai/libur.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Pegawai\User\DispenController;
 use App\Http\Controllers\Api\Pegawai\User\LiburController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,12 @@ Route::group([
     Route::get('/index', [LiburController::class, 'index']);
     Route::get('/month', [LiburController::class, 'month']);
     Route::post('/store', [LiburController::class, 'store']);
+});
+Route::group([
+    'middleware' => 'auth:api',
+    'prefix' => 'dispen'
+], function () {
+    Route::get('/pegawai', [DispenController::class, 'index']);
 });
 Route::group([
     'middleware' => 'auth:api',
