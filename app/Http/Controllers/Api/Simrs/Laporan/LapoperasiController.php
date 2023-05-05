e9be90167632f36806ea4a6854218a4d03adbaf4
kentangkotak <ariezlnd69@gmail.com>
2023-05-05 22:06:21 +0700

    xxxx

--- app/Http/Controllers/Api/Simrs/Laporan/LapoperasiController.php
+++ app/Http/Controllers/Api/Simrs/Laporan/LapoperasiController.php
@@ -3,11 +3,8 @@
 namespace App\Http\Controllers\Api\Simrs\Laporan;
 
 use App\Http\Controllers\Controller;
-use App\Models\KunjunganPoli;
 use App\Models\Simrs\Laporan\Operasi\LaporanOperasi;
-use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class LapoperasiController extends Controller
 {
@@ -16,13 +13,13 @@ class LapoperasiController extends Controller
         $from=request('from');
         $to=request('to');
         $query = LaporanOperasi::with([
-            'permintaanoperasi:rs1',
+            'permintaanoperasi:rs1,rs217.rs3 as tanggal',
             'pasien_kunjungan_poli:rs15.rs1 as norm,rs15.rs2 as nama',
             'pasien_kunjungan_rawat_inap:rs15.rs1 as norm,rs15.rs2 as nama'])
             //->whereMonth('rs217.rs3','='.$bln)
             //->whereYear('rs3','='. $thn)
             ->whereBetween('rs217.rs3', [$from, $to])
-            ->paginate(10);
+            ->get();
 
         // $query = LaporanOperasi::with(['permintaanoperasi:rs1'])
         // ->limit(100)->get();
