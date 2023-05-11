97b9d0f5612c60199eba11d89b4d6d4d443b42be
duro06 <andi.meka.025@gmail.com>
2023-05-12 00:16:38 +0700

    metode baru

--- app/Http/Controllers/Api/settings/AksesUserController.php
+++ app/Http/Controllers/Api/settings/AksesUserController.php
@@ -14,15 +14,15 @@ class AksesUserController extends Controller
     }
     public function storeAkses(Request $request)
     {
-        $anu = [];
-        foreach ($request->app as $app) {
-            foreach ($app['menus'] as $menus) {
-                foreach ($menus['submenus'] as $sub) {
-                    array_push($anu, [$app['id'], $menus['id'], $sub]);
-                }
-            }
-        }
-        $data = $anu;
+        // $anu = [];
+        // foreach ($request->app as $app) {
+        //     foreach ($app['menus'] as $menus) {
+        //         foreach ($menus['submenus'] as $sub) {
+        //             array_push($anu, [$app['id'], $menus['id'], $sub]);
+        //         }
+        //     }
+        // }
+        $data = $request->all();
         return new JsonResponse($data);
     }
 }
