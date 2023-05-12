6115bd21f0bab9edd988c1a59b17eca5e7c5bf1a
duro06 <andi.meka.025@gmail.com>
2023-05-12 07:21:49 +0700

    Akses crud OK

--- app/Http/Controllers/Api/settings/AksesUserController.php
+++ app/Http/Controllers/Api/settings/AksesUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\settings;
 
 use App\Http\Controllers\Controller;
+use App\Models\Pegawai\Akses\AksesUser;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -14,15 +15,46 @@ class AksesUserController extends Controller
     }
     public function storeAkses(Request $request)
     {
-        // $anu = [];
-        // foreach ($request->app as $app) {
-        //     foreach ($app['menus'] as $menus) {
-        //         foreach ($menus['submenus'] as $sub) {
-        //             array_push($anu, [$app['id'], $menus['id'], $sub]);
-        //         }
-        //     }
-        // }
-        $data = $request->all();
+        $data = [];
+        if ($request->tipe === true) {
+            foreach ($request->data as $anu) {
+                $wew = $this->createAkses($anu);
+                array_push($data, $wew);
+            }
+        } else {
+            foreach ($request->data as $anu) {
+                $wew = $this->deleteAkses($anu);
+                array_push($data, $wew);
+            }
+        }
+        // $data = $request->all();
         return new JsonResponse($data);
+    }
+    private function createAkses($uncal)
+    {
+        // return $uncal;
+        $data = AksesUser::firstOrCreate(
+            [
+                'user_id' => $uncal['user_id'],
+                'aplikasi_id' => $uncal['aplikasi_id'],
+                'menu_id' => $uncal['menu_id'],
+                'submenu_id' => $uncal['submenu_id'],
+            ]
+        );
+        return $data;
+    }
+    private function deleteAkses($uncal)
+    {
+        $data = AksesUser::where('user_id', $uncal['user_id'])
+            ->where('aplikasi_id', $uncal['aplikasi_id'])
+            ->where('menu_id', $uncal['menu_id'])
+            ->where('submenu_id', $uncal['submenu_id'])
+            ->first();
+        if ($data) {
+            $data->delete();
+            return $data;
+        }
+
+        return false;
     }
 }
