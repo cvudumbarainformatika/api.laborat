66f81a086d3a65b5f445dd57de90ef554cf4f470
kentangkotak <ariezlnd69@gmail.com>
2023-05-23 23:56:22 +0700

    bridgingmasterbpjs1

--- app/Http/Controllers/Api/Simrs/master/BridgingbpjsController.php
+++ app/Http/Controllers/Api/Simrs/master/BridgingbpjsController.php
@@ -8,11 +8,18 @@ use Illuminate\Http\Request;
 
 class BridgingbpjsController extends Controller
 {
-    public function cekpsertabpjs(Request $request)
+    public function cekpsertabpjsbynoka(Request $request)
     {
         $cekpsereta = BridgingbpjsHelper::get_url('vclaim','Peserta/nokartu/'. $request->noka.'/tglSEP/'.$request->tglsep);
-        $wew = $cekpsereta['result']->peserta->provUmum;
-        return ($wew);
+       // $wew = $cekpsereta['result']->peserta->provUmum;
+        return ($cekpsereta);
+    }
+
+    public function cekpsertabpjsbynik(Request $request)
+    {
+        $cekpseretax = BridgingbpjsHelper::get_url('vclaim','Peserta/nik/'. $request->nik.'/tglSEP/'.$request->tglsep);
+       // $wew = $cekpsereta['result']->peserta->provUmum;
+        return ($cekpseretax);
     }
 
     public function listrujukanpcare(Request $request)
@@ -29,7 +36,18 @@ class BridgingbpjsController extends Controller
 
     public function diagnosabybpjs(Request $request)
     {
-        $diagnosa = BridgingbpjsHelper::get_url('vclaim','referensi/diagnosa/'. $request->kodediagnosa);
-        return($diagnosa);
+        if($request->kodediagnosa != '')
+        {
+            $diagnosa = BridgingbpjsHelper::get_url('vclaim','referensi/diagnosa/'. $request->kodediagnosa);
+            return($diagnosa);
+        }
+            $diagnosa = BridgingbpjsHelper::get_url('vclaim','referensi/diagnosa/'. $request->diagnosa);
+            return($diagnosa);
+    }
+
+    public function faskesasalbpjs(Request $request)
+    {
+        $faskesbpjs = BridgingbpjsHelper::get_url('vclaim','referensi/faskes/'.$request->faskesasal.'/1');
+
     }
 }
--- routes/v1/simrs/pendaftaran/rajalumum/pendrajalbpjs.php
+++ routes/v1/simrs/pendaftaran/rajalumum/pendrajalbpjs.php
@@ -10,9 +10,11 @@ Route::group([
     // 'middleware' => 'jwt.verify',
     'prefix' => 'simrs/pendaftaran'
 ], function () {
-    Route::post('/cekpesertabpjs', [BridgingbpjsController::class, 'cekpsertabpjs']);
+    Route::post('/cekpsertabpjsbynoka', [BridgingbpjsController::class, 'cekpsertabpjsbynoka']);
+    Route::post('/cekpsertabpjsbynik', [BridgingbpjsController::class, 'cekpsertabpjsbynik']);
     Route::post('/listrujukanpcare', [BridgingbpjsController::class, 'listrujukanpcare']);
     Route::post('/listrujukanrs', [BridgingbpjsController::class, 'listrujukanrs']);
     Route::post('/listsepmrs', [listsepController::class, 'listsepmrs']);
     Route::post('/diagnosabybpjs', [BridgingbpjsController::class, 'diagnosabybpjs']);
+    Route::post('/faskesasalbpjs', [BridgingbpjsController::class, 'faskesasalbpjs']);
 });
