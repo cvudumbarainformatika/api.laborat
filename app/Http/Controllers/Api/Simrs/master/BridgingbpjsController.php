b98915d8f23f21f31ee7dcc3231b33ed3c2fd80e
kentangkotak <ariezlnd69@gmail.com>
2023-05-24 15:54:31 +0700

    jeniskartukarcis

--- app/Http/Controllers/Api/Simrs/master/BridgingbpjsController.php
+++ app/Http/Controllers/Api/Simrs/master/BridgingbpjsController.php
@@ -12,26 +12,26 @@ class BridgingbpjsController extends Controller
     {
         $cekpsereta = BridgingbpjsHelper::get_url('vclaim','Peserta/nokartu/'. $request->noka.'/tglSEP/'.$request->tglsep);
        // $wew = $cekpsereta['result']->peserta->provUmum;
-        return ($cekpsereta);
+        return $cekpsereta;
     }
 
     public function cekpsertabpjsbynik(Request $request)
     {
         $cekpseretax = BridgingbpjsHelper::get_url('vclaim','Peserta/nik/'. $request->nik.'/tglSEP/'.$request->tglsep);
        // $wew = $cekpsereta['result']->peserta->provUmum;
-        return ($cekpseretax);
+        return $cekpseretax;
     }
 
     public function listrujukanpcare(Request $request)
     {
         $listrujukanpcare = BridgingbpjsHelper::get_url('vclaim','Rujukan/List/Peserta/'. $request->noka);
-        return($listrujukanpcare);
+        return $listrujukanpcare;
     }
 
     public function listrujukanrs(Request $request)
     {
         $listrujukanrs = BridgingbpjsHelper::get_url('vclaim', '/Rujukan/RS/List/Peserta/'. $request->noka);
-        return($listrujukanrs);
+        return $listrujukanrs;
     }
 
     public function diagnosabybpjs(Request $request)
@@ -39,15 +39,15 @@ class BridgingbpjsController extends Controller
         if($request->kodediagnosa != '')
         {
             $diagnosa = BridgingbpjsHelper::get_url('vclaim','referensi/diagnosa/'. $request->kodediagnosa);
-            return($diagnosa);
+            return $diagnosa;
         }
             $diagnosa = BridgingbpjsHelper::get_url('vclaim','referensi/diagnosa/'. $request->diagnosa);
-            return($diagnosa);
+            return $diagnosa;
     }
 
     public function faskesasalbpjs(Request $request)
     {
         $faskesbpjs = BridgingbpjsHelper::get_url('vclaim','referensi/faskes/'.$request->faskesasal.'/1');
-
+        return $faskesbpjs;
     }
 }
