26df087204c4cba2148ff5718e72da9cc6cb67fa
duro06 <andi.meka.025@gmail.com>
2023-05-24 15:55:37 +0700

    anu

--- app/Http/Controllers/Api/Simrs/master/BridgingbpjsController.php
+++ app/Http/Controllers/Api/Simrs/master/BridgingbpjsController.php
@@ -4,50 +4,51 @@ namespace App\Http\Controllers\Api\Simrs\Master;
 
 use App\Helpers\BridgingbpjsHelper;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class BridgingbpjsController extends Controller
 {
     public function cekpsertabpjsbynoka(Request $request)
     {
-        $cekpsereta = BridgingbpjsHelper::get_url('vclaim','Peserta/nokartu/'. $request->noka.'/tglSEP/'.$request->tglsep);
-       // $wew = $cekpsereta['result']->peserta->provUmum;
+        $cekpsereta = BridgingbpjsHelper::get_url('vclaim', 'Peserta/nokartu/' . $request->noka . '/tglSEP/' . $request->tglsep);
+        // $wew = $cekpsereta['result']->peserta->provUmum;
         return ($cekpsereta);
     }
 
     public function cekpsertabpjsbynik(Request $request)
     {
-        $cekpseretax = BridgingbpjsHelper::get_url('vclaim','Peserta/nik/'. $request->nik.'/tglSEP/'.$request->tglsep);
-       // $wew = $cekpsereta['result']->peserta->provUmum;
+        $cekpseretax = BridgingbpjsHelper::get_url('vclaim', 'Peserta/nik/' . $request->nik . '/tglSEP/' . $request->tglsep);
+        // $wew = $cekpsereta['result']->peserta->provUmum;
         return ($cekpseretax);
     }
 
     public function listrujukanpcare(Request $request)
     {
-        $listrujukanpcare = BridgingbpjsHelper::get_url('vclaim','Rujukan/List/Peserta/'. $request->noka);
-        return($listrujukanpcare);
+        $listrujukanpcare = BridgingbpjsHelper::get_url('vclaim', 'Rujukan/List/Peserta/' . $request->noka);
+        return ($listrujukanpcare);
     }
 
     public function listrujukanrs(Request $request)
     {
-        $listrujukanrs = BridgingbpjsHelper::get_url('vclaim', '/Rujukan/RS/List/Peserta/'. $request->noka);
-        return($listrujukanrs);
+        $listrujukanrs = BridgingbpjsHelper::get_url('vclaim', '/Rujukan/RS/List/Peserta/' . $request->noka);
+        return ($listrujukanrs);
     }
 
     public function diagnosabybpjs(Request $request)
     {
-        if($request->kodediagnosa != '')
-        {
-            $diagnosa = BridgingbpjsHelper::get_url('vclaim','referensi/diagnosa/'. $request->kodediagnosa);
-            return($diagnosa);
+        if ($request->kodediagnosa != '') {
+            $diagnosa = BridgingbpjsHelper::get_url('vclaim', 'referensi/diagnosa/' . $request->kodediagnosa);
+            return ($diagnosa);
         }
-            $diagnosa = BridgingbpjsHelper::get_url('vclaim','referensi/diagnosa/'. $request->diagnosa);
-            return($diagnosa);
+        $diagnosa = BridgingbpjsHelper::get_url('vclaim', 'referensi/diagnosa/' . $request->diagnosa);
+        return ($diagnosa);
     }
 
     public function faskesasalbpjs(Request $request)
     {
-        $faskesbpjs = BridgingbpjsHelper::get_url('vclaim','referensi/faskes/'.$request->faskesasal.'/1');
-
+        // return new JsonResponse($request);
+        $faskesbpjs = BridgingbpjsHelper::get_url('vclaim', 'referensi/faskes/' . $request->faskesasal . '/1');
+        return $faskesbpjs;
     }
 }
