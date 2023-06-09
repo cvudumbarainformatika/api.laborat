c170fe12adcef4aee1809dbbff39358daf5bd771
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-06-10 02:49:07 +0700

    ok

---
+++ app/Http/Controllers/Api/Aplikasi/AplikasiController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api\Aplikasi;
+
+use App\Http\Controllers\Controller;
+use App\Models\aplikasi\Aplikasi;
+use App\Models\Pegawai\Akses\Access;
+use App\Models\Pegawai\Akses\Menu;
+use App\Models\Sigarang\Pegawai;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+
+class AplikasiController extends Controller
+{
+    public function index()
+    {
+        $data = Aplikasi::with(['menus', 'menus.submenus'])->get();
+
+        return new JsonResponse($data);
+    }
+}
