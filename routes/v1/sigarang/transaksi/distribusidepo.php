737c70e84c723a41be6076af891bf358daad59cb
duro06 <andi.meka.025@gmail.com>
2023-06-10 23:07:46 +0700

    distribusi depo done

--- routes/v1/sigarang/transaksi/distribusidepo.php
+++ routes/v1/sigarang/transaksi/distribusidepo.php
@@ -16,6 +16,7 @@ Route::group([
     Route::post('/store', [DistribusiDepoController::class, 'store']);
     Route::post('/new-store', [DistribusiDepoController::class, 'newStore']);
     Route::post('/save-detail', [DistribusiDepoController::class, 'saveDetail']);
+    Route::post('/status-penerimaan', [DistribusiDepoController::class, 'gantiStatusPenerimaan']);
     Route::post('/terima-depo', [DistribusiDepoController::class, 'diterimaDepo']);
     Route::post('/hapus-data-gudang', [DistribusiDepoController::class, 'hapusDataStokGudang']);
     Route::get('/destroy', [DistribusiDepoController::class, 'destroy']);
