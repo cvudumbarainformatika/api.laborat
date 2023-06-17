a765b88fdf69d3752980ceafb87ad9d5deb21911
kentangkotak <ariezlnd69@gmail.com>
2023-06-17 12:15:53 +0700

    master obat new

---
+++ app/Models/Simrs/Penunjang/Farmasinew/Mobatnew.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\Simrs\Penunjang\Farmasinew;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Mobatnew extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+    protected $table = 'new_masterobat';
+    protected $guarded = ['id'];
+}
