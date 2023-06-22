c5185e957f59315a670a82741ce6c8c6e093878a
kentangkotak <ariezlnd69@gmail.com>
2023-06-22 09:36:52 +0700

    wew

--- app/Models/Simrs/Penunjang/Farmasinew/Mmerk.php
+++ app/Models/Simrs/Penunjang/Farmasinew/Mmerk.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Mmerk extends Model
 {
     use HasFactory,SoftDeletes;
-    protected $table = 'Mmerk';
+    protected $table = 'mmerk';
     protected $guarded = ['id'];
     protected $connection = 'farmasi';
 }
