ad7357c872d1369d8e567e46eb3ddd56e9c2f9de
kentangkotak <ariezlnd69@gmail.com>
2023-06-26 12:06:45 +0700

    wew lagi

---
+++ app/Http/Controllers/Api/Simrs/Antrian/AntrianController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api\Simrs\Antrian;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AntrianController extends Controller
+{
+    // public function call_layanan_ruang()
+    // {
+    //     $call_antrianruang =
+    // }
+}
