94a02b32616607df0d6e3389267fbbd3a16e23fa
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-07-01 17:07:00 +0700

    ggfg

--- app/Http/Controllers/Api/Simrs/Pendaftaran/Rajal/DaftarrajalController.php
+++ app/Http/Controllers/Api/Simrs/Pendaftaran/Rajal/DaftarrajalController.php
@@ -20,63 +20,65 @@ use PhpParser\Node\Stmt\TryCatch;
 class DaftarrajalController extends Controller
 {
 
-    public static function simpanMpasien($request){
+    public static function simpanMpasien($request)
+    {
         $request->validate([
             'norm' => 'required|string|max:6|min:6',
             'tglmasuk' => 'required|date_format:Y-m-d H:i:s',
             'nik' => 'string|max:16|min:16',
             'tgllahir' => 'required|date_format:Y-m-d'
         ]);
-        $masterpasien = Mpasien::updateOrCreate(['rs1' => $request->norm],
-                [
-                    'rs2' => $request->nama,
-                    'rs3' => $request->sapaan,
-                    'rs4' => $request->alamat,
-                    'alamatdomisili' =>$request->alamatdomisili,
-                    'rs5' => $request->kelurahan,
-                    'kd_kel' =>$request->kodekelurahan,
-                    'rs6' => $request->kecamatan,
-                    'kd_kec' => $request->kodekecamatan,
-                    'rs7' => $request->rt,
-                    'rs8' => $request->rw,
-                    'rs10' => $request->propinsi,
-                    'kd_propinsi' => $request->kodepropinsi,
-                    'rs11' => $request->kabupatenkota,
-                    'kd_kota' =>$request->kodekabupatenkota,
-                    'rs49' => $request->nik,
-                    'rs37' => $request->templahir,
-                    'rs16' => $request->tgllahir,
-                    'rs17' => $request->kelamin,
-                    'rs19' => $request->pendidikan,
-                    'kd_kelamin' => $request->kodekelamin,
-                    'rs22' => $request->agama,
-                    'kd_agama' => $request->kodemapagama,
-                    'rs39' => $request->suku,
-                    'rs55' => $request->noteleponhp,
-                    'bahasa' => $request->bahasa,
-                    'noidentitaslain' => $request->nomoridentitaslain,
-                    'namaibu' => $request->namaibukandung,
-                    'kodepos' => $request->kodepos,
-                    'kd_negara' => $request->negara,
-                    'kd_rt_dom' => $request->rtdomisili,
-                    'kd_rw_dom' => $request->rwdomisili,
-                    'kd_kel_dom' => $request->kelurahandomisili,
-                    'kd_kec_dom' => $request->kecamatandomisili,
-                    'kd_kota_dom' => $request->kabupatenkotadomisili,
-                    'kodeposdom' => $request->kodeposdomisili,
-                    'kd_prov_dom' => $request->propinsidomisili,
-                    'kd_negara_dom' => $request->negaradomisili,
-                    'noteleponrumah' => $request->noteleponrumah,
-                    'kd_pendidikan' => $request->kodependidikan,
-                    'kd_pekerjaan' => $request->pekerjaan,
-                    'flag_pernikahan' => $request->statuspernikahan,
-                    'rs46' => $request->nokabpjs,
-                    'rs40' => $request->barulama,
-                    'gelardepan' => $request->gelardepan,
-                    'gelarbelakang' => $request->gelarbelakang
-                ]
-            );
-            return $masterpasien;
+        $masterpasien = Mpasien::updateOrCreate(
+            ['rs1' => $request->norm],
+            [
+                'rs2' => $request->nama,
+                'rs3' => $request->sapaan,
+                'rs4' => $request->alamat,
+                'alamatdomisili' => $request->alamatdomisili,
+                'rs5' => $request->kelurahan,
+                'kd_kel' => $request->kodekelurahan,
+                'rs6' => $request->kecamatan,
+                'kd_kec' => $request->kodekecamatan,
+                'rs7' => $request->rt,
+                'rs8' => $request->rw,
+                'rs10' => $request->propinsi,
+                'kd_propinsi' => $request->kodepropinsi,
+                'rs11' => $request->kabupatenkota,
+                'kd_kota' => $request->kodekabupatenkota,
+                'rs49' => $request->nik,
+                'rs37' => $request->templahir,
+                'rs16' => $request->tgllahir,
+                'rs17' => $request->kelamin,
+                'rs19' => $request->pendidikan,
+                'kd_kelamin' => $request->kodekelamin,
+                'rs22' => $request->agama,
+                'kd_agama' => $request->kodemapagama,
+                'rs39' => $request->suku,
+                'rs55' => $request->noteleponhp,
+                'bahasa' => $request->bahasa,
+                'noidentitaslain' => $request->nomoridentitaslain,
+                'namaibu' => $request->namaibukandung,
+                'kodepos' => $request->kodepos,
+                'kd_negara' => $request->negara,
+                'kd_rt_dom' => $request->rtdomisili,
+                'kd_rw_dom' => $request->rwdomisili,
+                'kd_kel_dom' => $request->kelurahandomisili,
+                'kd_kec_dom' => $request->kecamatandomisili,
+                'kd_kota_dom' => $request->kabupatenkotadomisili,
+                'kodeposdom' => $request->kodeposdomisili,
+                'kd_prov_dom' => $request->propinsidomisili,
+                'kd_negara_dom' => $request->negaradomisili,
+                'noteleponrumah' => $request->noteleponrumah,
+                'kd_pendidikan' => $request->kodependidikan,
+                'kd_pekerjaan' => $request->pekerjaan,
+                'flag_pernikahan' => $request->statuspernikahan,
+                'rs46' => $request->nokabpjs,
+                'rs40' => $request->barulama,
+                'gelardepan' => $request->gelardepan,
+                'gelarbelakang' => $request->gelarbelakang
+            ]
+        );
+        return $masterpasien;
     }
 
     public static function simpankunjunganpoli($request)
@@ -84,68 +86,66 @@ class DaftarrajalController extends Controller
         $tglmasukx = Carbon::create($request->tglmasuk);
         $tglmasuk = $tglmasukx->toDateString();
         $cekpoli = KunjunganPoli::where('rs2', $request->norm)
-        ->where('rs8', $request->kodepoli)
-        ->whereDate('rs3', $tglmasuk)
-        ->count();
+            ->where('rs8', $request->kodepoli)
+            ->whereDate('rs3', $tglmasuk)
+            ->count();
 
-        if($cekpoli > 0)
-        {
+        if ($cekpoli > 0) {
             // return new JsonResponse(['message' => 'PASIEN SUDAH ADA DI HARI DAN POLI YANG SAMA'], 500);
             return false;
         }
 
         DB::select('call reg_rajal(@nomor)');
-            $hcounter = DB::table('rs1')->select('rs13')->get();
-            $wew = $hcounter[0]->rs13;
-            $noreg = FormatingHelper::gennoreg($wew,'J');
+        $hcounter = DB::table('rs1')->select('rs13')->get();
+        $wew = $hcounter[0]->rs13;
+        $noreg = FormatingHelper::gennoreg($wew, 'J');
 
-            $input = new Request( [
-                'noreg' => $noreg
-            ]);
+        $input = new Request([
+            'noreg' => $noreg
+        ]);
 
-            $input->validate([
-                'noreg' => 'required|unique:rs17,rs1'
-            ]);
+        $input->validate([
+            'noreg' => 'required|unique:rs17,rs1'
+        ]);
 
         //   $wew =  Validator::make($input, [
         //         'noreg' => 'unique:rs17,rs1'
         //     ]);
 
-           $simpankunjunganpoli = KunjunganPoli::create([
-                'rs1' => $input->noreg,
-                'rs2' => $request->norm,
-                'rs3' => $request->tglmasuk,
-                'rs6' => $request->asalrujukan,
-                'rs8' => $request->kodepoli,
-                //'rs9' => $request->dpjp,
-                'rs10' => 0,
-                'rs11' => '',
-                'rs12' => 0,
-                'rs13' => 0,
-                'rs14' => $request->sistembayar,
-                'rs15' => $request->karcis,
-                'rs18' => auth()->user()->pegawai_id,
-                'rs20' => 'Pendaftaran',
+        $simpankunjunganpoli = KunjunganPoli::create([
+            'rs1' => $input->noreg,
+            'rs2' => $request->norm,
+            'rs3' => $request->tglmasuk,
+            'rs6' => $request->asalrujukan,
+            'rs8' => $request->kodepoli,
+            //'rs9' => $request->dpjp,
+            'rs10' => 0,
+            'rs11' => '',
+            'rs12' => 0,
+            'rs13' => 0,
+            'rs14' => $request->sistembayar,
+            'rs15' => $request->karcis,
+            'rs18' => auth()->user()->pegawai_id,
+            'rs20' => 'Pendaftaran',
 
-            ]);
+        ]);
         return [
-            'simpan'=>$simpankunjunganpoli?'':$simpankunjunganpoli,
-            'input'=>$input,
-            'masuk'=>$tglmasuk,
-            'count'=>$cekpoli
+            'simpan' => $simpankunjunganpoli ? '' : $simpankunjunganpoli,
+            'input' => $input,
+            'masuk' => $tglmasuk,
+            'count' => $cekpoli
         ];
     }
 
-    public static function simpankarcis($request,$input)
+    public static function simpankarcis($request, $input)
     {
-        $kode_biaya=explode('#',$request->kode_biaya);
-        $nama_biaya=explode('#',$request->nama_biaya);
-        $sarana=explode('#',$request->sarana);
-        $pelayanan=explode('#',$request->pelayanan);
+        $kode_biaya = explode('#', $request->kode_biaya);
+        $nama_biaya = explode('#', $request->nama_biaya);
+        $sarana = explode('#', $request->sarana);
+        $pelayanan = explode('#', $request->pelayanan);
 
-        $anu=[];
-        foreach($kode_biaya as $key => $value)
-        {
+        $anu = [];
+        foreach ($kode_biaya as $key => $value) {
             // $xxx = new Karcispoli();
             // $xxx->kode_biaya = $value;
             // $xxx->nama_biaya = $nama_biaya[$key];
@@ -153,27 +153,28 @@ class DaftarrajalController extends Controller
             // $xxx->pelayanan = $pelayanan[$key];
 
             // if()
-            $kar=Karcispoli::firstOrCreate(
+            $kar = Karcispoli::firstOrCreate(
                 [
                     'rs2' => $request->norm,
                     'rs4' => $request->tglmasuk,
-                    'rs3' => $value.'#',
+                    'rs3' => $value . '#',
                 ],
                 [
-                'rs1' => $input,
-                // 'rs3' => $xxx->kode_biaya,
-                'rs5' => 'D',
-                 'rs6' => $nama_biaya[$key],
-                'rs7' => $sarana[$key],
-                'rs8' => $request->sistembayar,
-                'rs10' => 'userenrty',
-                // 'rs11' => $xxx->pelayanan,
-                'rs11' => $pelayanan[$key],
-                'rs12' => 'userentry',
-                'rs13' => '1'
-            ]);
-            if($kar){
-                array_push($anu,$kar);
+                    'rs1' => $input,
+                    // 'rs3' => $xxx->kode_biaya,
+                    'rs5' => 'D',
+                    'rs6' => $nama_biaya[$key],
+                    'rs7' => $sarana[$key],
+                    'rs8' => $request->sistembayar,
+                    'rs10' => 'userenrty',
+                    // 'rs11' => $xxx->pelayanan,
+                    'rs11' => $pelayanan[$key],
+                    'rs12' => 'userentry',
+                    'rs13' => '1'
+                ]
+            );
+            if ($kar) {
+                array_push($anu, $kar);
             }
         }
         return $anu;
@@ -186,42 +187,42 @@ class DaftarrajalController extends Controller
             DB::beginTransaction();
 
             //-----------Masuk Transaksi--------------
-           // $user = auth()->user(]);
-           $masterpasien=$this->simpanMpasien($request);
-           $simpankunjunganpoli=$this->simpankunjunganpoli($request);
-           if($simpankunjunganpoli){
-            $karcis=$this->simpankarcis($request,$simpankunjunganpoli['input']->noreg);
-           }
+            // $user = auth()->user(]);
+            $masterpasien = $this->simpanMpasien($request);
+            $simpankunjunganpoli = $this->simpankunjunganpoli($request);
+            if ($simpankunjunganpoli) {
+                $karcis = $this->simpankarcis($request, $simpankunjunganpoli['input']->noreg);
+            }
 
 
             DB::commit();
-            return new JsonResponse([
-                'message' => 'DATA TERSIMPAN...!!!',
-                'noreg' => $simpankunjunganpoli?$simpankunjunganpoli['input']->noreg:'gagal',
-                'cek' => $simpankunjunganpoli?$simpankunjunganpoli['count']:'gagal',
-                'masuk' => $simpankunjunganpoli?$simpankunjunganpoli['masuk']:'gagal',
-                'hasil' => $simpankunjunganpoli?$simpankunjunganpoli['simpan']:'gagal',
-                'karcis' => $karcis?$karcis:'gagal',
-                'master' => $masterpasien,
-            ],
-                200);
+            return new JsonResponse(
+                [
+                    'message' => 'DATA TERSIMPAN...!!!',
+                    'noreg' => $simpankunjunganpoli ? $simpankunjunganpoli['input']->noreg : 'gagal',
+                    'cek' => $simpankunjunganpoli ? $simpankunjunganpoli['count'] : 'gagal',
+                    'masuk' => $simpankunjunganpoli ? $simpankunjunganpoli['masuk'] : 'gagal',
+                    'hasil' => $simpankunjunganpoli ? $simpankunjunganpoli['simpan'] : 'gagal',
+                    'karcis' => $karcis ? $karcis : 'gagal',
+                    'master' => $masterpasien,
+                ],
+                200
+            );
         } catch (\Exception $th) {
             //throw $th;
             DB::rollBack();
-            return response()->json(['Gagal tersimpan'=>$th],500);
+            return response()->json(['Gagal tersimpan' => $th], 500);
         }
-
     }
 
     public function daftarkunjunganpasienbpjs()
     {
-        if(!request('tgl'))
-        {
+        if (request('tgl') === '' || request('tgl') === null) {
             $tgl = Carbon::now()->format('Y-m-d 00:00:00');
             $tglx = Carbon::now()->format('Y-m-d 23:59:59');
-        }else{
-            $tgl = request('tgl').' 00:00:00';
-            $tglx = request('tgl').' 23:59:59';
+        } else {
+            $tgl = request('tgl') . ' 00:00:00';
+            $tglx = request('tgl') . ' 23:59:59';
         }
         $daftarkunjunganpasienbpjs = KunjunganPoli::select(
             'rs1 as noreg',
@@ -230,9 +231,12 @@ class DaftarrajalController extends Controller
             'rs8',
             'rs9',
             'rs14'
-        )
-        ->with(['masterpasien' => function($q)
-            {
+        )->with([
+            'msistembayar:rs1,rs2 as sistembayar,rs9 as groupsistembayar',
+            'relmpoli:rs1,rs2 as namapoli,rs4',
+            'dokter:rs1,rs2 as dokter',
+            'seprajal:rs1,rs8 as sep',
+            'masterpasien' => function ($q) {
                 $q->select([
                     'rs1',
                     DB::raw('concat(rs3," ",gelardepan," ",rs2," ",gelarbelakang) as nama'),
@@ -249,17 +253,19 @@ class DaftarrajalController extends Controller
                     'rs55 as nohp'
                 ]);
             }
-        ])->with([
-            'msistembayar:rs1,rs2 as sistembayar,rs9 as groupsistembayar',
-            'relmpoli:rs1,rs2 as namapoli,rs4',
-            'dokter:rs1,rs2 as dokter',
-            'seprajal:rs1,rs8 as sep'
+
         ])
-        ->whereBetween('rs17.rs3', [$tgl, $tglx])
-        ->where('rs17.rs8','!=', 'POL014')
-        ->where('rs17.rs14','Like', '%BPJS%')
-        ->paginate(10);
+            // ->with([
+            //     'msistembayar:rs1,rs2 as sistembayar,rs9 as groupsistembayar',
+            //     'relmpoli:rs1,rs2 as namapoli,rs4',
+            //     'dokter:rs1,rs2 as dokter',
+            //     'seprajal:rs1,rs8 as sep'
+            // ])
+            ->whereBetween('rs3', [$tgl, $tglx]) // ini default hari ini
+            ->where('rs8', '<>', 'POL014')
+            ->where('rs14', 'Like', '%BPJS%')
+            ->orderBy('rs3', 'DESC')
+            ->paginate(request('per_page'));
         return new JsonResponse($daftarkunjunganpasienbpjs);
     }
-
 }
