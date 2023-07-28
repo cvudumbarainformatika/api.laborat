a1c1215575a14106c0be0d14635c595c0c00ecff
kentangkotak <ariezlnd69@gmail.com>
2023-07-28 10:24:17 +0700

    wew

--- routes/v1/simrs/rajal/poli.php
+++ routes/v1/simrs/rajal/poli.php
@@ -1,15 +1,16 @@
 <?php
 
 use App\Http\Controllers\Api\Simrs\Rajal\PoliController as RajalPoliController;
-use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Route;
 
-// Route::group([
-//     'middleware' => 'auth:api',
-//     // 'middleware' => 'jwt.verify',
-//     'prefix' => 'simrs/rajal/poli'
-// ], function(){
-//     Route::get('/kunjunganpoli', [RajalPoliController::class, 'kunjunganpoli']);
-// });
+Route::group([
+    'middleware' => 'auth:api',
+    // 'middleware' => 'jwt.verify',
+    'prefix' => 'simrs/rajal/poli'
+], function () {
+   // Route::get('/listminmaxobat', [MinmaxobatController::class, 'listminmaxobat']);
+    Route::get('/kunjunganpoli', [RajalPoliController::class, 'kunjunganpoli']);
+});
 
 
 
