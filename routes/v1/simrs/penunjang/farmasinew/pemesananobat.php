f310aa0aa28188ac274459857594efbd0fc421a8
kentangkotak <ariezlnd69@gmail.com>
2023-08-21 15:21:01 +0700

    pihak ketiga

--- routes/v1/simrs/penunjang/farmasinew/pemesananobat.php
+++ routes/v1/simrs/penunjang/farmasinew/pemesananobat.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\Logistik\Sigarang\Transaksi\PemesananController;
 use App\Http\Controllers\Api\Simrs\Penunjang\Farmasinew\Pemesanan\DialogrencanapemesananController;
 use App\Http\Controllers\Api\Simrs\Penunjang\Farmasinew\Pemesanan\PemesananController as PemesananPemesananController;
+use App\Http\Controllers\Api\Simrs\Penunjang\Farmasinew\PihakketigaController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -15,4 +16,5 @@ Route::group([
     Route::get('/dialogrencanabeli_rinci', [DialogrencanapemesananController::class, 'dialogrencanabeli_rinci']);
     Route::post('/simpanpemesanan', [PemesananPemesananController::class, 'simpan']);
     Route::get('/listpemesanan', [PemesananPemesananController::class, 'listpemesanan']);
+    Route::get('/pihakketiga', [PihakketigaController::class, 'pihakketiga']);
 });
