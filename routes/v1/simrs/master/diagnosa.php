15a6b4fcf326e8cc150186e4a71eaeb7f6212137
kentangkotak <ariezlnd69@gmail.com>
2023-08-25 08:57:58 +0700

    diagnosa

--- routes/v1/simrs/master/diagnosa.php
+++ routes/v1/simrs/master/diagnosa.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\Simrs\Master\DiagnosaController;
-use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Route;
 
 Route::group([
     'middleware' => 'auth:api',
