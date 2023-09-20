fd87fd15255b0b7f347b29a23655b59e6e6a88a3
kentangkotak <ariezlnd69@gmail.com>
2023-09-20 11:00:43 +0700

    wesssss

--- app/Http/Controllers/Api/Simrs/Penunjang/Farmasinew/Gudang/DistribusigudangController.php
+++ app/Http/Controllers/Api/Simrs/Penunjang/Farmasinew/Gudang/DistribusigudangController.php
@@ -7,6 +7,7 @@ use App\Models\Simrs\Penunjang\Farmasinew\Depo\Permintaandepoheder;
 use App\Models\Simrs\Penunjang\Farmasinew\Depo\Permintaandeporinci;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DistribusigudangController extends Controller
 {
@@ -15,7 +16,16 @@ class DistribusigudangController extends Controller
         $gudang = request('kdgudang');
         $nopermintaan = request('no_permintaan');
         if ($gudang === '' || $gudang === null) {
-            $listpermintaandepo = Permintaandepoheder::with('permintaanrinci.masterobat')
+            $listpermintaandepo = Permintaandepoheder::with(
+                [
+                    'permintaanrinci.masterobat',
+                    'user:id,nip,nama',
+                    'permintaanrinci.stokreal' => function ($stokdendiri) {
+                        $stokdendiri->select('kdobat', 'kdobat', DB::raw('sum(stokreal.jumlah) as stokdendiri'))
+                            ->groupBy('kdruang');
+                    }
+                ]
+            )
                 ->where('no_permintaan', 'Like', '%' . $nopermintaan . '%')
                 ->where('flag', '1')
                 ->orderBY('tgl_permintaan', 'desc')
@@ -23,7 +33,7 @@ class DistribusigudangController extends Controller
             return new JsonResponse($listpermintaandepo);
         } else {
 
-            $listpermintaandepo = Permintaandepoheder::with('permintaanrinci.masterobat')
+            $listpermintaandepo = Permintaandepoheder::with(['permintaanrinci.masterobat', 'user:id,nip,nama'])
                 ->where('no_permintaan', 'Like', '%' . $nopermintaan . '%')
                 ->where('tujuan', $gudang)
                 ->where('flag', '1')
