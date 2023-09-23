a89df84e7c77f2b2381e7390cef3bb4a6c17b8b7
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-09-23 17:56:12 +0700

    dffg

--- routes/v1/simrs/penunjang/laborat/laborat.php
+++ routes/v1/simrs/penunjang/laborat/laborat.php
@@ -12,10 +12,7 @@ Route::group([
     Route::get('/dialoglaboratpoli', [LaboratController::class, 'listmasterpemeriksaanpoli']);
     Route::get('/getnota', [LaboratController::class, 'getnota']);
     Route::post('/simpanpermintaanlaborat', [LaboratController::class, 'simpanpermintaanlaborat']);
-<<<<<<< HEAD
     Route::post('/hapuspermintaanlaborat', [LaboratController::class, 'hapuspermintaanlaborat']);
-=======
 
     Route::get('/listmasterpemeriksaanradiologi', [RadiologimetaController::class, 'listmasterpemeriksaanradiologi']);
->>>>>>> 6e2b30650f16fb3860c6ad5ac4241e045e9833a7
 });
