cf77ef88187ec5724623c6a07f21c47576b25f1c
kentangkotak <ariezlnd69@gmail.com>
2023-09-26 11:15:10 +0700

    wewew

---
+++ routes/v1/simrs/penunjang/ok/ok.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\Api\Simrs\Penunjang\Kamaroperasi\KamaroperasiController;
+use Illuminate\Support\Facades\Route;
+
+Route::group([
+    'middleware' => 'auth:api',
+    // 'middleware' => 'jwt.verify',
+    'prefix' => 'simrs/penunjang/ok'
+], function () {
+    Route::get('/getnota', [KamaroperasiController::class, 'getnota']);
+    Route::post('/permintaanoperasi', [KamaroperasiController::class, 'permintaanoperasi']);
+    Route::post('/hapuspermintaanok', [KamaroperasiController::class, 'hapuspermintaanok']);
+});
