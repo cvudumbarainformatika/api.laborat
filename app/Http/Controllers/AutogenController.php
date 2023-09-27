c5e2e827546bf26c9ed86b49adb6b2b8264ba5eb
duro06 <andi.meka.025@gmail.com>
2023-09-27 10:16:51 +0700

    sasaaa

--- app/Http/Controllers/AutogenController.php
+++ app/Http/Controllers/AutogenController.php
@@ -1927,9 +1927,9 @@ class AutogenController extends Controller
         // return new JsonResponse(['data' => $distribute]);
         // }
         $data = RecentStokUpdate::select(
-            // 'barang_r_s.nama',
-            // 'barang_r_s.kode',
-            // 'barang_r_s.kode_satuan',
+            'barang_r_s.nama',
+            'barang_r_s.kode',
+            'barang_r_s.kode_satuan',
             'recent_stok_updates.id',
             'recent_stok_updates.kode_rs',
             'recent_stok_updates.kode_ruang',
@@ -1945,18 +1945,18 @@ class AutogenController extends Controller
             //     '=',
             //     'penerimaans.no_penerimaan'
             // )
-            // ->join('barang_r_s', 'recent_stok_updates.kode_rs', '=', 'barang_r_s.kode')
+            ->join('barang_r_s', 'recent_stok_updates.kode_rs', '=', 'barang_r_s.kode')
             // ->join('satuans', 'satuans.kode', '=', 'barang_r_s.kode_satuan')
             ->where('recent_stok_updates.kode_ruang', $ruang)
             ->where('recent_stok_updates.sisa_stok', '>', 0)
-            // ->when(request('q'), function ($search) {
-            //     $search->where(function ($anu) {
-            //         $anu->where('barang_r_s.nama', 'LIKE', '%' . request('q') . '%')
-            //             ->orWhere('barang_r_s.kode', 'LIKE', '%' . request('q') . '%');
-            //     })
-            //         ->where('barang_r_s.tipe', request('tipe'));
-            // })
-            // ->where('barang_r_s.tipe', request('tipe'))
+            ->when(request('q'), function ($search) {
+                $search->where(function ($anu) {
+                    $anu->where('barang_r_s.nama', 'LIKE', '%' . request('q') . '%')
+                        ->orWhere('barang_r_s.kode', 'LIKE', '%' . request('q') . '%');
+                })
+                    ->where('barang_r_s.tipe', request('tipe'));
+            })
+            ->where('barang_r_s.tipe', request('tipe'))
             // ->orderBy('penerimaans.tanggal', 'ASC')
             // ->with([
             //     'detaildistribusilangsung' => function ($detail) {
