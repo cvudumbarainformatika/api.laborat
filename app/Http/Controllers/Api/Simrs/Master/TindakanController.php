4e3631d3a817f25ba19f02fd3e8bfc8d942392cc
kentangkotak <ariezlnd69@gmail.com>
2023-10-09 09:36:42 +0700

    ffffff

---
+++ app/Http/Controllers/Api/Simrs/Master/TindakanController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api\Simrs\Master;
+
+use App\Http\Controllers\Controller;
+use App\Models\Simrs\Tindakan\Tindakan;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class TindakanController extends Controller
+{
+    public function listtindakan()
+    {
+        $listtindakan = Tindakan::select(
+            'rs1 as kodetindakan',
+            'rs2 as nmtindkan',
+            'rs8 as js3',
+            'rs9 as jp3',
+            'rs11 as js2',
+            'rs12 as jp2',
+            'rs14 as js1',
+            'rs15 as jp1',
+            'rs17 as jsutama',
+            'rs18 as jputama',
+            'rs20 as jsvip',
+            'rs23 as jpvvip',
+            'rs24 as jpvvip'
+        )
+            ->paginate(request('perpage'));
+        return new JsonResponse($listtindakan);
+    }
+}
---
+++ routes/v1/simrs/master/tindakan.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Api\Simrs\Master\TindakanController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::group([
+    'middleware' => 'auth:api',
+    // 'middleware' => 'jwt.verify',
+    'prefix' => 'simrs/master'
+], function () {
+    Route::get('/listtindakan', [TindakanController::class, 'listtindakan']);
+});
