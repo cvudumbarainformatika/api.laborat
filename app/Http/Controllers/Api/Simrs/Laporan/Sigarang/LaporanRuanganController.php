74038efa98ddd0b3b6ca007ea8308e27a3a1ec1c
duro06 <andi.meka.025@gmail.com>
2023-10-24 12:22:18 +0700

    filter pengeluaran depo

--- app/Http/Controllers/Api/Simrs/Laporan/Sigarang/LaporanRuanganController.php
+++ app/Http/Controllers/Api/Simrs/Laporan/Sigarang/LaporanRuanganController.php
@@ -38,9 +38,7 @@ class LaporanRuanganController extends Controller
             ->whereIn('permintaanruangans.status', [7, 8])
             ->where('detail_permintaanruangans.jumlah_distribusi', '>', 0)
             ->when(request('kode_ruang'), function ($q) {
-                if (request('kode_ruang') !== 'Gd-02010102') {
-                    $q->where('permintaanruangans.dari', request('kode_ruang'));
-                }
+                $q->where('permintaanruangans.dari', request('kode_ruang'));
             })
             ->when(request('q'), function ($q) {
                 $anu = BarangRS::select('kode')->where('barang_r_s.kode', 'LIKE', '%' . request('q') . '%')
@@ -52,9 +50,9 @@ class LaporanRuanganController extends Controller
             ->leftJoin('distribusi_langsungs', function ($p) {
                 $p->on('distribusi_langsungs.id', '=', 'detail_distribusi_langsungs.distribusi_langsung_id');
             })
-            ->when(request('kode_ruang') === 'Gd-02010102', function ($q) {
-                $q->where('distribusi_langsungs.ruang_tujuan', request('kode_ruang'));
-            })
+            // ->when(request('kode_ruang') === 'Gd-02010102', function ($q) {
+            //     $q->where('distribusi_langsungs.ruang_tujuan', request('kode_ruang'));
+            // })
             ->when(request('q'), function ($q) {
                 $anu = BarangRS::select('kode')->where('barang_r_s.kode', 'LIKE', '%' . request('q') . '%')
                     ->orWhere('barang_r_s.nama', 'LIKE', '%' . request('q') . '%')->get();
@@ -62,64 +60,137 @@ class LaporanRuanganController extends Controller
             })
             ->whereBetween('distribusi_langsungs.tanggal', [request('from') . ' 00:00:00', request('to') . ' 23:59:59'])
             ->get();
-        $result = BarangRS::select([
-            'permintaanruangans.tanggal',
-            'distribusi_langsungs.tanggal as tanggal_l',
-            'detail_permintaanruangans.no_penerimaan',
-            'detail_permintaanruangans.kode_rs',
-            'detail_distribusi_langsungs.kode_rs as kode_rs_l',
-            'barang_r_s.nama',
-            'satuans.nama as satuan',
-            'ruangs.uraian as tujuan',
-            DB::raw('ROUND(sum(detail_distribusi_langsungs.jumlah),2) as jumlah_distribusi_l'),
-            DB::raw('ROUND(sum(detail_permintaanruangans.jumlah),2) as jumlah'),
-            DB::raw('ROUND(sum(detail_permintaanruangans.jumlah_disetujui),2) as jumlah_disetujui'),
-            DB::raw('ROUND(sum(detail_permintaanruangans.jumlah_distribusi),2) as jumlah_distribusi'),
-        ])
-            ->leftJoin('detail_permintaanruangans', function ($b) {
+
+
+        if (request('kode_ruang') === 'Gd-02010102') {
+            $result = BarangRS::select([
+                'distribusi_langsungs.tanggal',
+                'detail_distribusi_langsungs.kode_rs',
+                'detail_distribusi_langsungs.no_penerimaan',
+                'barang_r_s.nama',
+                'satuans.nama as satuan',
+                'ruangs.uraian as tujuan',
+                DB::raw('ROUND(sum(detail_distribusi_langsungs.jumlah),2) as jumlah_distribusi'),
+            ])
+                ->leftJoin('detail_distribusi_langsungs', function ($b) {
+                    $b->on('detail_distribusi_langsungs.kode_rs', '=', 'barang_r_s.kode')
+                        ->leftJoin('distribusi_langsungs', function ($p) {
+                            $p->on('distribusi_langsungs.id', '=', 'detail_distribusi_langsungs.distribusi_langsung_id');
+                        });
+                })
+                ->leftJoin('ruangs', function ($p) {
+                    $p->on('ruangs.kode', '=', 'distribusi_langsungs.ruang_tujuan');
+                })->leftJoin('satuans', function ($s) {
+                    $s->on('satuans.kode', '=', 'barang_r_s.kode_satuan');
+                })
+
+                ->where(function ($q) use ($dist) {
+                    $q->whereBetween('distribusi_langsungs.tanggal', [request('from') . ' 00:00:00', request('to') . ' 23:59:59'])
+                        ->whereIn('barang_r_s.kode', $dist);
+                })
+                ->groupBy(
+                    'barang_r_s.kode',
+                    'distribusi_langsungs.tanggal',
+                    'distribusi_langsungs.ruang_tujuan'
+                )->orderBy('ruangs.uraian', 'ASC')
+                ->orderBy('distribusi_langsungs.tanggal', 'ASC')
+                ->withTrashed()
+                ->paginate(request('per_page'));
+            return new JsonResponse($result);
+        } else if (request('kode_ruang') !== 'Gd-02010102') {
+            $result = BarangRS::select([
+                'permintaanruangans.tanggal',
+                'detail_permintaanruangans.no_penerimaan',
+                'detail_permintaanruangans.kode_rs',
+                'barang_r_s.nama',
+                'satuans.nama as satuan',
+                'ruangs.uraian as tujuan',
+                DB::raw('ROUND(sum(detail_permintaanruangans.jumlah),2) as jumlah'),
+                DB::raw('ROUND(sum(detail_permintaanruangans.jumlah_disetujui),2) as jumlah_disetujui'),
+                DB::raw('ROUND(sum(detail_permintaanruangans.jumlah_distribusi),2) as jumlah_distribusi'),
+            ])->leftJoin('detail_permintaanruangans', function ($b) {
                 $b->on('detail_permintaanruangans.kode_rs', '=', 'barang_r_s.kode')
                     ->leftJoin('permintaanruangans', function ($p) {
                         $p->on('permintaanruangans.id', '=', 'detail_permintaanruangans.permintaanruangan_id');
                     });
-            })
-            ->leftJoin('detail_distribusi_langsungs', function ($b) {
-                $b->on('detail_distribusi_langsungs.kode_rs', '=', 'barang_r_s.kode')
-                    ->leftJoin('distribusi_langsungs', function ($p) {
-                        $p->on('distribusi_langsungs.id', '=', 'detail_distribusi_langsungs.distribusi_langsung_id');
-                    });
-            })
-            ->leftJoin('ruangs', function ($p) {
-                $p->on('ruangs.kode', '=', 'detail_permintaanruangans.tujuan')
-                    ->orOn('ruangs.kode', '=', 'distribusi_langsungs.ruang_tujuan');
-            })
-            ->leftJoin('satuans', function ($s) {
+            })->leftJoin('ruangs', function ($p) {
+                $p->on('ruangs.kode', '=', 'detail_permintaanruangans.tujuan');
+            })->leftJoin('satuans', function ($s) {
                 $s->on('satuans.kode', '=', 'barang_r_s.kode_satuan');
             })
-            ->where(function ($q) use ($minta) {
-                $q->whereBetween('permintaanruangans.tanggal', [request('from') . ' 00:00:00', request('to') . ' 23:59:59'])
-                    ->where('detail_permintaanruangans.jumlah_distribusi', '>', 0)
-                    ->whereIn('barang_r_s.kode', $minta);
-            })
-            ->orWhere(function ($q) use ($dist) {
-                $q->whereBetween('distribusi_langsungs.tanggal', [request('from') . ' 00:00:00', request('to') . ' 23:59:59'])
-                    ->whereIn('barang_r_s.kode', $dist);
-            })
-            ->groupBy(
-                'barang_r_s.kode',
-                'permintaanruangans.tanggal',
-                'distribusi_langsungs.tanggal',
-                'detail_permintaanruangans.tujuan',
-                'distribusi_langsungs.ruang_tujuan'
-            )
-            ->orderBy('ruangs.uraian', 'ASC')
-            ->orderBy('permintaanruangans.tanggal', 'ASC')
-            ->orderBy('distribusi_langsungs.tanggal', 'ASC')
-            ->withTrashed()
-            // ->get();
-            ->paginate(request('per_page'));
-        $data = $result;
 
-        return new JsonResponse($data);
+                ->where(function ($q) use ($minta) {
+                    $q->whereBetween('permintaanruangans.tanggal', [request('from') . ' 00:00:00', request('to') . ' 23:59:59'])
+                        ->where('detail_permintaanruangans.jumlah_distribusi', '>', 0)
+                        ->whereIn('barang_r_s.kode', $minta);
+                })
+                ->groupBy(
+                    'barang_r_s.kode',
+                    'permintaanruangans.tanggal',
+                    'detail_permintaanruangans.tujuan'
+                )->orderBy('ruangs.uraian', 'ASC')
+                ->orderBy('permintaanruangans.tanggal', 'ASC')
+                ->withTrashed()
+                ->paginate(request('per_page'));
+            return new JsonResponse($result);
+        } else {
+
+            $result = BarangRS::select([
+                'permintaanruangans.tanggal',
+                'distribusi_langsungs.tanggal as tanggal_l',
+                'detail_permintaanruangans.no_penerimaan',
+                'detail_permintaanruangans.kode_rs',
+                'detail_distribusi_langsungs.kode_rs as kode_rs_l',
+                'barang_r_s.nama',
+                'satuans.nama as satuan',
+                'ruangs.uraian as tujuan',
+                DB::raw('ROUND(sum(detail_distribusi_langsungs.jumlah),2) as jumlah_distribusi_l'),
+                DB::raw('ROUND(sum(detail_permintaanruangans.jumlah),2) as jumlah'),
+                DB::raw('ROUND(sum(detail_permintaanruangans.jumlah_disetujui),2) as jumlah_disetujui'),
+                DB::raw('ROUND(sum(detail_permintaanruangans.jumlah_distribusi),2) as jumlah_distribusi'),
+            ])
+                ->leftJoin('detail_permintaanruangans', function ($b) {
+                    $b->on('detail_permintaanruangans.kode_rs', '=', 'barang_r_s.kode')
+                        ->leftJoin('permintaanruangans', function ($p) {
+                            $p->on('permintaanruangans.id', '=', 'detail_permintaanruangans.permintaanruangan_id');
+                        });
+                })
+                ->leftJoin('detail_distribusi_langsungs', function ($b) {
+                    $b->on('detail_distribusi_langsungs.kode_rs', '=', 'barang_r_s.kode')
+                        ->leftJoin('distribusi_langsungs', function ($p) {
+                            $p->on('distribusi_langsungs.id', '=', 'detail_distribusi_langsungs.distribusi_langsung_id');
+                        });
+                })
+                ->leftJoin('ruangs', function ($p) {
+                    $p->on('ruangs.kode', '=', 'detail_permintaanruangans.tujuan')
+                        ->orOn('ruangs.kode', '=', 'distribusi_langsungs.ruang_tujuan');
+                })
+                ->leftJoin('satuans', function ($s) {
+                    $s->on('satuans.kode', '=', 'barang_r_s.kode_satuan');
+                })
+                ->where(function ($q) use ($minta) {
+                    $q->whereBetween('permintaanruangans.tanggal', [request('from') . ' 00:00:00', request('to') . ' 23:59:59'])
+                        ->where('detail_permintaanruangans.jumlah_distribusi', '>', 0)
+                        ->whereIn('barang_r_s.kode', $minta);
+                })
+                ->orWhere(function ($q) use ($dist) {
+                    $q->whereBetween('distribusi_langsungs.tanggal', [request('from') . ' 00:00:00', request('to') . ' 23:59:59'])
+                        ->whereIn('barang_r_s.kode', $dist);
+                })
+                ->groupBy(
+                    'barang_r_s.kode',
+                    'permintaanruangans.tanggal',
+                    'distribusi_langsungs.tanggal',
+                    'detail_permintaanruangans.tujuan',
+                    'distribusi_langsungs.ruang_tujuan'
+                )
+                ->orderBy('ruangs.uraian', 'ASC')
+                ->orderBy('permintaanruangans.tanggal', 'ASC')
+                ->orderBy('distribusi_langsungs.tanggal', 'ASC')
+                ->withTrashed()
+                ->paginate(request('per_page'));
+            return new JsonResponse($result);
+        }
     }
     public function lapPemakaianRuangan()
     {
