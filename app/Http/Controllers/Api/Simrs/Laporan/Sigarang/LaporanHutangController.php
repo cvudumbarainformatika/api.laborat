6ffef6515f67236d427486ef1e3d9c76f28dda29
duro06 <andi.meka.025@gmail.com>
2023-10-27 22:07:58 +0700

    Laporan hutang

--- app/Http/Controllers/Api/Simrs/Laporan/Sigarang/LaporanHutangController.php
+++ app/Http/Controllers/Api/Simrs/Laporan/Sigarang/LaporanHutangController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api\Simrs\Laporan\Sigarang;
 
 use App\Http\Controllers\Controller;
+use App\Models\Sigarang\Supplier;
+use App\Models\Sigarang\Transaksi\Penerimaan\Penerimaan;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -10,6 +12,18 @@ class LaporanHutangController extends Controller
 {
     public function lapHutang()
     {
-        return new JsonResponse(request()->all());
+        $kodePer = Penerimaan::select('kode_perusahaan')->where('nilai_pembayaran', '<=', 0)->distinct('kode_perusahaan')->get();
+
+        $data = Supplier::select('kode', 'nama')
+            ->with([
+                'penerimaan' => function ($p) {
+                    $p->select('kode_perusahaan', 'no_penerimaan', 'faktur', 'tanggal', 'tanggal_bast', 'tempo', 'total', 'nilai_tagihan')
+                        // ->selectRaw('sum(total) as sumTotal')
+                        // ->selectRaw('sum(nilai_tagihan) as sumTagihan')
+                        ->where('nilai_pembayaran', '<=', 0);
+                }
+            ])
+            ->whereIn('kode', $kodePer)->get();
+        return new JsonResponse($data);
     }
 }
--- app/Models/Sigarang/Supplier.php
+++ app/Models/Sigarang/Supplier.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Sigarang;
 
+use App\Models\Sigarang\Transaksi\Penerimaan\Penerimaan;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,6 +13,10 @@ class Supplier extends Model
     protected $table = 'pihak_ketiga';
     protected $fillable = [];
 
+    public function penerimaan()
+    {
+        return $this->hasMany(Penerimaan::class, 'kode_perusahaan', 'kode');
+    }
     public function scopeFilter($search, array $reqs)
     {
         $search->when($reqs['q'] ?? false, function ($search, $query) {
