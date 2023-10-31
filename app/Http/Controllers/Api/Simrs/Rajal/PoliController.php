23d0745c35061170ae4312d106a0a571e6b6dfda
kentangkotak <ariezlnd69@gmail.com>
2023-10-31 15:42:16 +0700

    sasa

--- app/Http/Controllers/Api/Simrs/Rajal/PoliController.php
+++ app/Http/Controllers/Api/Simrs/Rajal/PoliController.php
@@ -163,13 +163,14 @@ class PoliController extends Controller
 
     public function terimapasien(Request $request)
     {
-        // $ceksep = Seprajal::where('rs1', $request->noreg)->count();
-        // if ($ceksep > 0) {
-        $updatekunjungan = KunjunganPoli::where('rs1', $request->noreg)->first();
-        $updatekunjungan->rs19 = '2';
-        $updatekunjungan->save();
-        return new JsonResponse(['message' => 'ok'], 200);
-        // }
+        $cekx = KunjunganPoli::where('rs1', $request->noreg)->first();
+        $flag = $cekx->rs19;
+        if ($flag === '') {
+            $updatekunjungan = KunjunganPoli::where('rs1', $request->noreg)->first();
+            $updatekunjungan->rs19 = '2';
+            $updatekunjungan->save();
+            return new JsonResponse(['message' => 'ok'], 200);
+        }
         //  return new JsonResponse([''], 500);
     }
 
