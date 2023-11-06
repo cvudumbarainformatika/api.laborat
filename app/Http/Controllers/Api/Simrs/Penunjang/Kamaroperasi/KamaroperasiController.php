db20e375df6792e1acb0de341f2829e0b8a8f1eb
kentangkotak <ariezlnd69@gmail.com>
2023-11-06 15:07:13 +0700

    wew

--- app/Http/Controllers/Api/Simrs/Penunjang/Kamaroperasi/KamaroperasiController.php
+++ app/Http/Controllers/Api/Simrs/Penunjang/Kamaroperasi/KamaroperasiController.php
@@ -68,7 +68,7 @@ class KamaroperasiController extends Controller
         }
         // $hapusdetail = PermintaanOperasi::where('rs2', '=', $cari->nota)->delete();
         $hapus = $cari->delete();
-        $nota = PermintaanOperasi::select('rs2')->where('rs1', $request->noreg)
+        $nota = PermintaanOperasi::select('rs2 as nota')->where('rs1', $request->noreg)
             ->groupBy('rs2')->orderBy('id', 'DESC')->get();
         if (!$hapus) {
             return new JsonResponse(['message' => 'gagal dihapus'], 500);
