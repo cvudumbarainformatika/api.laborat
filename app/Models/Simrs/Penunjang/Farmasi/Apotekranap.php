d0a9177527192cf92210e6435c65fb6998ed694e
kentangkotak <ariezlnd69@gmail.com>
2023-11-28 11:25:35 +0700

    wew

--- app/Models/Simrs/Penunjang/Farmasi/Apotekranap.php
+++ app/Models/Simrs/Penunjang/Farmasi/Apotekranap.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Simrs\Penunjang\Farmasi;
 
+use App\Models\Simrs\Master\Mobat;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
