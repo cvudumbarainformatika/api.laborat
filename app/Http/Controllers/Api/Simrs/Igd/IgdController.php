7b69a17a58b014eb21080a0c38f63901ae2df1a6
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-12-08 08:41:27 +0700

    ghjyhgf

--- app/Http/Controllers/Api/Simrs/Igd/IgdController.php
+++ app/Http/Controllers/Api/Simrs/Igd/IgdController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\Api\Simrs\Igd;
 
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class IgdController extends Controller
 {
--- app/Http/Controllers/Api/Simrs/Ranap/RanapController.php
+++ app/Http/Controllers/Api/Simrs/Ranap/RanapController.php
@@ -3,11 +3,25 @@
 namespace App\Http\Controllers\Api\Simrs\Ranap;
 
 use App\Http\Controllers\Controller;
+use App\Models\Simrs\Ranap\Kunjunganranap;
+use Carbon\Carbon;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class RanapController extends Controller
 {
     public function kunjunganpasien()
     {
-        return 'ok';
+        $ruangan = request('koderuangan');
+
+        if (request('to') === '' || request('from') === null) {
+            $tgl = Carbon::now()->format('Y-m-d 00:00:00');
+            $tglx = Carbon::now()->format('Y-m-d 23:59:59');
+        } else {
+            $tgl = request('to') . ' 00:00:00';
+            $tglx = request('from') . ' 23:59:59';
+        }
+        $data = Kunjunganranap::whereBetween('rs17.rs3', [$tgl, $tglx])->get();
     }
 }
