2b3eb8d4f7c26c9143e77c43d4c393c808849704
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-12-08 08:25:59 +0700

    dsfdsf

---
+++ app/Http/Controllers/Api/Simrs/Igd/IgdController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api\Simrs\Igd;
+
+use App\Http\Controllers\Controller;
+
+class IgdController extends Controller
+{
+    public function kunjunganpasien()
+    {
+        return 'ok';
+    }
+}
---
+++ app/Http/Controllers/Api/Simrs/Ranap/RanapController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api\Simrs\Ranap;
+
+use App\Http\Controllers\Controller;
+
+class RanapController extends Controller
+{
+    public function kunjunganpasien()
+    {
+        return 'ok';
+    }
+}
