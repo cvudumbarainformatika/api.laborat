671634a0ccb2ab16b787ef7a000f90d2f0ffef9c
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2024-01-01 14:24:52 +0700

    fghd

--- app/Models/Simrs/Penunjang/Farmasinew/Stok/Stokopname.php
+++ app/Models/Simrs/Penunjang/Farmasinew/Stok/Stokopname.php
@@ -2,10 +2,7 @@
 
 namespace App\Models\Simrs\Penunjang\Farmasinew\Stok;
 
-<<<<<<< HEAD
 use App\Models\Simrs\Penunjang\Farmasinew\Mobatnew;
-=======
->>>>>>> 13b8704a5e960cb39cf0516b65f95c8f3cd7a761
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,12 +12,9 @@ class Stokopname extends Model
     protected $table = 'stokopname';
     protected $guarded = ['id'];
     protected $connection = 'farmasi';
-<<<<<<< HEAD
 
     public function masterobat()
     {
         return $this->hasOne(Mobatnew::class, 'kd_obat', 'kdobat');
     }
-=======
->>>>>>> 13b8704a5e960cb39cf0516b65f95c8f3cd7a761
 }
