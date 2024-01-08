47116cb06274871896b82f25ac0cbd218284078a
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2024-01-08 12:24:14 +0700

    sadasd

--- app/Helpers/AuthSatsetHelper.php
+++ app/Helpers/AuthSatsetHelper.php
@@ -14,6 +14,8 @@ class AuthSatsetHelper
         $url_dev = 'https://api-satusehat-dev.dto.kemkes.go.id/oauth2/v1/accesstoken?grant_type=client_credentials';
         $url_staging = 'https://api-satusehat-stg.dto.kemkes.go.id/oauth2/v1/accesstoken?grant_type=client_credentials';
         $url_prod = 'https://api-satusehat.kemkes.go.id/oauth2/v1/accesstoken?grant_type=client_credentials';
+
+
         $client_id = '8Sy0DMwjAfINN24Wa22u0YcieLLc71bSmGkGqCFsDBcyhG1r';
         $client_secret = 'mj5cQtOjlkhGdK3nOl1YcGyAFx92WTWtALbdPJZIVMfFXDXGCSS6D35HZeWONwFJ';
 
@@ -37,8 +39,8 @@ class AuthSatsetHelper
         $url_staging = 'https://api-satusehat-stg.dto.kemkes.go.id/oauth2/v1/accesstoken?grant_type=client_credentials';
         $url_prod = 'https://api-satusehat.kemkes.go.id/oauth2/v1/accesstoken?grant_type=client_credentials';
 
-        $client_id = '8Sy0DMwjAfINN24Wa22u0YcieLLc71bSmGkGqCFsDBcyhG1r';
-        $client_secret = 'mj5cQtOjlkhGdK3nOl1YcGyAFx92WTWtALbdPJZIVMfFXDXGCSS6D35HZeWONwFJ';
+        $client_id = '9wvWiOVMkOTBAixmVqufdAK5MVQ7aXRLDRG8f70abw7nDXFp';
+        $client_secret = 'GNxqqQJGim11FAr3243vWMLpgAUxBsPOGik0wBhkVH3BbrpAXuz6uz8MkvsQ0vzF';
 
         $headers = ['Content-Type' => 'application/x-www-form-urlencoded'];
 
