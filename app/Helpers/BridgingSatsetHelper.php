01213343bf59f19cf985487608b70f67baa4a254
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2024-01-15 08:41:58 +0700

    dsfds

--- app/Helpers/BridgingSatsetHelper.php
+++ app/Helpers/BridgingSatsetHelper.php
@@ -22,6 +22,11 @@ class BridgingSatsetHelper
 
         return $url_prod;
     }
+    public static function organization_id()
+    {
+        $organization_id = '100026342';
+        return $organization_id;
+    }
 
     public static function get_data($token, $params)
     {
@@ -47,6 +52,22 @@ class BridgingSatsetHelper
             ];
             return $send;
         }
+
+        if ($data['resourceType'] === 'Bundle' && $data['total'] === 0) {
+            $err = [
+                'method' => 'GET',
+                'url' => $params,
+                'response' => $data
+            ];
+            $resp = SatsetErrorRespon::create($err);
+            $send = [
+                'message' => 'Tidak Ditemukan Data di Satu Sehat',
+                'data' => $resp
+            ];
+            return $send;
+        }
+
+
         // JIKA SUCCESS
         $success = [
             'method' => 'GET',
@@ -55,18 +76,16 @@ class BridgingSatsetHelper
 
         ];
 
-        if ($data['resourceType'] === 'Bundle') {
-            if (count($data['entry']) > 0) {
-                $resp = Satset::firstOrCreate([
-                    'resource' => $data['resourceType'],
-                    'uuid' => $data['entry'][0]['resource']['id']
-                ], $success);
-                $send = [
-                    'message' => 'success',
-                    'data' => $resp
-                ];
-                return $send;
-            }
+        if ($data['resourceType'] === 'Bundle' && $data['total'] > 0) {
+            $resp = Satset::firstOrCreate([
+                'resource' => $data['resourceType'],
+                'uuid' => $data['entry'][0]['resource']['id']
+            ], $success);
+            $send = [
+                'message' => 'success',
+                'data' => $resp
+            ];
+            return $send;
         }
     }
 
@@ -144,6 +163,48 @@ class BridgingSatsetHelper
         $resp = Satset::updateOrCreate([
             'resource' => $data['resourceType'],
             'uuid' => $data['id']
+        ], $success);
+        $send = [
+            'message' => 'success',
+            'data' => $resp
+        ];
+        return $send;
+    }
+
+
+    public static function post_bundle($token, $form, $noreg)
+    {
+        $url = self::base_url();
+        $response = Http::withToken($token)->post($url, $form);
+        $data = json_decode($response, true);
+        // return $data;
+        // JIKA ERROR
+        $error = $data['resourceType'] === 'OperationOutcome';
+        if ($error) {
+            $err = [
+                'method' => 'POST',
+                'url' => $url,
+                'response' => $data,
+                'uuid' => $noreg
+            ];
+            $resp = SatsetErrorRespon::create($err);
+
+            $send = [
+                'message' => 'failed',
+                'data' => $resp
+            ];
+            return $send;
+        }
+
+        // JIKA SUCCESS
+        $success = [
+            'method' => 'POST',
+            'url' => $url,
+            'response' => $data,
+        ];
+        $resp = Satset::firstOrCreate([
+            'resource' => $data['resourceType'],
+            'uuid' => $noreg
         ], $success);
         $send = [
             'message' => 'success',
--- app/Helpers/PostKunjunganHelper.php
+++ app/Helpers/PostKunjunganHelper.php
@@ -2,6 +2,7 @@
 
 namespace App\Helpers;
 
+use Carbon\Carbon;
 use Illuminate\Support\Str;
 
 class PostKunjunganHelper
@@ -17,12 +18,89 @@ class PostKunjunganHelper
         $encounter = self::generateUuid();
 
         $practitioner = $request->datasimpeg['satset_uuid'];
+
+        $taskid = collect($request->taskid);
+        if (count($taskid) === 0) {
+            return response()->json([
+                'message' => 'Maaf ... Pasien Ini Tidak Mempunyai TASK ID'
+            ], 500);
+        }
+
+        //   // 1 (mulai waktu tunggu admisi),
+        //   // 2 (akhir waktu tunggu admisi/mulai waktu layan admisi),
+        //   // 3 (akhir waktu layan admisi/mulai waktu tunggu poli),
+        //   // 4 (akhir waktu tunggu poli/mulai waktu layan poli),
+        //   // 5 (akhir waktu layan poli/mulai waktu tunggu farmasi),
+        //   // 6 (akhir waktu tunggu farmasi/mulai waktu layan farmasi membuat obat),
+        //   // 7 (akhir waktu obat selesai dibuat),
+        //   // 99 (tidak hadir/batal)
+
+        // $task3 = Carbon::parse($taskid[0]['created_at'])->locale('id');
+        $task3 = $taskid->filter(function ($item) {
+            return $item['taskid'] === '3';
+        })->first();
+        $task4 = $taskid->filter(function ($item) {
+            return $item['taskid'] === '4';
+        })->first();
+        $task5 = $taskid->filter(function ($item) {
+            return $item['taskid'] === '5';
+        })->first();
+
+        if (!$task3 || !$task4 || !$task5) {
+            return response()->json([
+                'message' => 'Maaf ... TASK iD Tdk lengkap'
+            ], 500);
+        }
+
+        // return Carbon::parse($task3['created_at']);
+
+        $antri = Carbon::parse($task3['created_at'])->toIso8601String();
+        $start = Carbon::parse($task4['created_at'])->toIso8601String();
+        $end = Carbon::parse($task5['created_at'])->toIso8601String();
+
+        setlocale(LC_ALL, 'IND');
+        $dt = Carbon::parse($request->tgl_kunjungan)->locale('id');
+        $dt->settings(['formatFunction' => 'translatedFormat']);
+        $tgl_kunjungan = $dt->format('l, j F Y');
+        // $tgl_kunjungan = $dt->format('l, j F Y ; h:i a');
+
+        $rajal_org = '4b8fb632-6435-4fc1-8ea0-7aacc39974d6';
+        $organization_id = BridgingSatsetHelper::organization_id();
+
+        // DIAGNOSA
+
+        $diagnosa = [];
+        foreach ($request->diagnosa as $key => $value) {
+            $uuid = self::generateUuid();
+            $data = [
+                "condition" => [
+                    "reference" => "urn:uuid:$uuid",
+                    "display" => $value['masterdiagnosa']['rs4']
+                ],
+                "use" => [
+                    "coding" => [
+                        [
+                            "system" => "http://terminology.hl7.org/CodeSystem/diagnosis-role",
+                            "code" => "DD",
+                            "display" => "Discharge diagnosis"
+                        ]
+                    ]
+                ],
+                "rank" => $key + 1
+            ];
+
+            $diagnosa[] = $data;
+        }
+
+
+        // return $antri;
         #Bundle #1
         $body =
             [
                 "resourceType" => "Bundle",
                 "type" => "transaction",
                 "entry" => [
+                    // ENCOUNTER
                     [
                         "fullUrl" => "urn:uuid:$encounter",
                         "resource" => [
@@ -34,7 +112,7 @@ class PostKunjunganHelper
                                 "display" => "ambulatory"
                             ],
                             "subject" => [
-                                "reference" => "Patient/$request->satset_uuid",
+                                "reference" => "Patient/$request->pasien_uuid",
                                 "display" => $request->nama
                             ],
                             "participant" => [
@@ -57,81 +135,88 @@ class PostKunjunganHelper
                                 ]
                             ],
                             "period" => [
-                                "start" => "2022-12-18T14:00:00+00:00",
-                                "end" => "2022-12-18T16:00:00+00:00"
+                                "start" => $antri,
+                                "end" => $end
                             ],
                             "location" => [
                                 [
                                     "location" => [
-                                        "reference" => "Location/b017aa54-f1df-4ec2-9d84-8823815d7228",
-                                        "display" => "Ruang 1A, Poliklinik Bedah Rawat Jalan Terpadu, Lantai 2, Gedung G"
+                                        "reference" => "Location/" . $request['relmpoli']['ruang']['satset_uuid'],
+                                        "display" => "Ruang " . $request['relmpoli']['ruang']['ruang'] . " " . $request['relmpoli']['panggil_antrian'] . ", RSUD Mohamad Saleh, Lantai " . $request['relmpoli']['ruang']['lantai'] . ", Gedung " . $request['relmpoli']['ruang']['gedung']
+                                        // "display" => $request['relmpoli']['ruang']['gedung']
                                     ]
                                 ]
                             ],
-                            "diagnosis" => [
-                                [
-                                    "condition" => [
-                                        "reference" => "urn:uuid:ba5a7dec-023f-45e1-adb9-1b9d71737a5f",
-                                        "display" => "Acute appendicitis, other and unspecified"
-                                    ],
-                                    "use" => [
-                                        "coding" => [
-                                            [
-                                                "system" => "http://terminology.hl7.org/CodeSystem/diagnosis-role",
-                                                "code" => "DD",
-                                                "display" => "Discharge diagnosis"
-                                            ]
-                                        ]
-                                    ],
-                                    "rank" => 1
-                                ],
-                                [
-                                    "condition" => [
-                                        "reference" => "urn:uuid:470fc62d-9ab1-4c90-8d24-fb245c105c59",
-                                        "display" => "Dengue haemorrhagic fever"
-                                    ],
-                                    "use" => [
-                                        "coding" => [
-                                            [
-                                                "system" => "http://terminology.hl7.org/CodeSystem/diagnosis-role",
-                                                "code" => "DD",
-                                                "display" => "Discharge diagnosis"
-                                            ]
-                                        ]
-                                    ],
-                                    "rank" => 2
-                                ]
-                            ],
+                            // "diagnosis" => [
+                            //     [
+                            //         "condition" => [
+                            //             "reference" => "urn:uuid:ba5a7dec-023f-45e1-adb9-1b9d71737a5f",
+                            //             "display" => "Acute appendicitis, other and unspecified"
+                            //         ],
+                            //         "use" => [
+                            //             "coding" => [
+                            //                 [
+                            //                     "system" => "http://terminology.hl7.org/CodeSystem/diagnosis-role",
+                            //                     "code" => "DD",
+                            //                     "display" => "Discharge diagnosis"
+                            //                 ]
+                            //             ]
+                            //         ],
+                            //         "rank" => 1
+                            //     ],
+                            //     [
+                            //         "condition" => [
+                            //             "reference" => "urn:uuid:470fc62d-9ab1-4c90-8d24-fb245c105c59",
+                            //             "display" => "Dengue haemorrhagic fever"
+                            //         ],
+                            //         "use" => [
+                            //             "coding" => [
+                            //                 [
+                            //                     "system" => "http://terminology.hl7.org/CodeSystem/diagnosis-role",
+                            //                     "code" => "DD",
+                            //                     "display" => "Discharge diagnosis"
+                            //                 ]
+                            //             ]
+                            //         ],
+                            //         "rank" => 2
+                            //     ]
+                            // ],
+
+                            "diagnosis" => $diagnosa,
                             "statusHistory" => [
                                 [
                                     "status" => "arrived",
                                     "period" => [
-                                        "start" => "2022-12-18T14:00:00+00:00",
-                                        "end" => "2022-12-18T15:00:00+00:00"
+                                        "start" => $antri,
+                                        "end" => $start
                                     ]
                                 ],
                                 [
                                     "status" => "in-progress",
                                     "period" => [
-                                        "start" => "2022-12-18T15:00:00+00:00",
-                                        "end" => "2022-12-18T16:00:00+00:00"
+                                        "start" => $start,
+                                        "end" => $end
                                     ]
                                 ],
                                 [
                                     "status" => "finished",
                                     "period" => [
-                                        "start" => "2022-12-18T16:00:00+00:00",
-                                        "end" => "2022-12-18T16:00:00+00:00"
+                                        "start" => $end,
+                                        "end" => $end
                                     ]
                                 ]
                             ],
                             "serviceProvider" => [
-                                "reference" => "Organization/10000004"
+                                // "reference" => "Organization/$organization_id"
+                                "reference" => "Organization/$organization_id"
                             ],
+
+                            // gak yakin
                             "identifier" => [
                                 [
-                                    "system" => "http://sys-ids.kemkes.go.id/encounter/1000004",
-                                    "value" => "P20240001"
+                                    "system" => "http://sys-ids.kemkes.go.id/encounter/$organization_id",
+                                    "value" => $request->pasien_uuid
+                                    // "value" => "P20240001"
                                 ]
                             ]
                         ],
@@ -140,102 +225,67 @@ class PostKunjunganHelper
                             "url" => "Encounter"
                         ]
                     ],
-                    [
-                        "fullUrl" => "urn:uuid:ba5a7dec-023f-45e1-adb9-1b9d71737a5f",
-                        "resource" => [
-                            "resourceType" => "Condition",
-                            "clinicalStatus" => [
-                                "coding" => [
-                                    [
-                                        "system" => "http://terminology.hl7.org/CodeSystem/condition-clinical",
-                                        "code" => "active",
-                                        "display" => "Active"
-                                    ]
-                                ]
-                            ],
-                            "category" => [
-                                [
-                                    "coding" => [
-                                        [
-                                            "system" => "http://terminology.hl7.org/CodeSystem/condition-category",
-                                            "code" => "encounter-diagnosis",
-                                            "display" => "Encounter Diagnosis"
-                                        ]
-                                    ]
-                                ]
-                            ],
-                            "code" => [
-                                "coding" => [
-                                    [
-                                        "system" => "http://hl7.org/fhir/sid/icd-10",
-                                        "code" => "K35.8",
-                                        "display" => "Acute appendicitis, other and unspecified"
-                                    ]
-                                ]
-                            ],
-                            "subject" => [
-                                "reference" => "Patient/100000030009",
-                                "display" => "Budi Santoso"
-                            ],
-                            "encounter" => [
-                                "reference" => "urn:uuid:0a26ca28-0ea3-486d-8fa9-6f9edd37e567",
-                                "display" => "Kunjungan Budi Santoso di hari Selasa, 18 Desember 2022"
-                            ]
-                        ],
-                        "request" => [
-                            "method" => "POST",
-                            "url" => "Condition"
-                        ]
-                    ],
-                    [
-                        "fullUrl" => "urn:uuid:470fc62d-9ab1-4c90-8d24-fb245c105c59",
-                        "resource" => [
-                            "resourceType" => "Condition",
-                            "clinicalStatus" => [
-                                "coding" => [
-                                    [
-                                        "system" => "http://terminology.hl7.org/CodeSystem/condition-clinical",
-                                        "code" => "active",
-                                        "display" => "Active"
-                                    ]
-                                ]
-                            ],
-                            "category" => [
-                                [
-                                    "coding" => [
-                                        [
-                                            "system" => "http://terminology.hl7.org/CodeSystem/condition-category",
-                                            "code" => "encounter-diagnosis",
-                                            "display" => "Encounter Diagnosis"
-                                        ]
-                                    ]
-                                ]
-                            ],
-                            "code" => [
-                                "coding" => [
-                                    [
-                                        "system" => "http://hl7.org/fhir/sid/icd-10",
-                                        "code" => "A91",
-                                        "display" => "Dengue haemorrhagic fever"
-                                    ]
-                                ]
-                            ],
-                            "subject" => [
-                                "reference" => "Patient/100000030009",
-                                "display" => "Budi Santoso"
-                            ],
-                            "encounter" => [
-                                "reference" => "urn:uuid:0a26ca28-0ea3-486d-8fa9-6f9edd37e567",
-                                "display" => "Kunjungan Budi Santoso di hari Selasa, 18 Desember 2022"
-                            ]
-                        ],
-                        "request" => [
-                            "method" => "POST",
-                            "url" => "Condition"
-                        ]
-                    ]
                 ]
             ];
+
+
+
+        //  CONDITION
+        foreach ($request->diagnosa as $key => $value) {
+            $cond =
+                [
+                    // "fullUrl" => "urn:uuid:ba5a7dec-023f-45e1-adb9-1b9d71737a5f",
+                    "fullUrl" => $diagnosa[$key]['condition']['reference'],
+                    "resource" => [
+                        "resourceType" => "Condition",
+                        "clinicalStatus" => [
+                            "coding" => [
+                                [
+                                    "system" => "http://terminology.hl7.org/CodeSystem/condition-clinical",
+                                    "code" => "active",
+                                    "display" => "Active"
+                                ]
+                            ]
+                        ],
+                        "category" => [
+                            [
+                                "coding" => [
+                                    [
+                                        "system" => "http://terminology.hl7.org/CodeSystem/condition-category",
+                                        "code" => "encounter-diagnosis",
+                                        "display" => "Encounter Diagnosis"
+                                    ]
+                                ]
+                            ]
+                        ],
+                        "code" => [
+                            "coding" => [
+                                [
+                                    "system" => "http://hl7.org/fhir/sid/icd-10",
+                                    "code" => $value['rs3'],
+                                    "display" => $value['masterdiagnosa']['rs4']
+                                ]
+                            ]
+                        ],
+                        "subject" => [
+                            "reference" => "Patient/$request->pasien_uuid",
+                            "display" => $request->nama
+                        ],
+                        "encounter" => [
+                            "reference" => "urn:uuid:$encounter",
+                            "display" => "Kunjungan $request->nama di hari $tgl_kunjungan"
+                        ]
+                    ],
+                    "request" => [
+                        "method" => "POST",
+                        "url" => "Condition"
+                    ]
+                ];
+
+            array_push($body['entry'], $cond);
+        }
+
+
         return $body;
     }
 }
