624d4b19a6a86ecf70a0894eefa0794e9403ec53
duro06 <andi.meka.025@gmail.com>
2024-02-16 06:35:50 +0700

    filter namaobat kodeobat

--- app/Http/Controllers/Api/Simrs/Penunjang/Farmasinew/Ruangan/PemakaianRuanganController.php
+++ app/Http/Controllers/Api/Simrs/Penunjang/Farmasinew/Ruangan/PemakaianRuanganController.php
@@ -17,6 +17,13 @@ class PemakaianRuanganController extends Controller
             ->with('obat:kd_obat,nama_obat,satuan_k', 'ruang:kode,uraian')
             ->where('jumlah', '>', 0)
             ->where('kdruang', request('kdruang'))
+            ->when(request('q'), function ($query) {
+                $kode = Mobatnew::select('kd_obat')
+                    ->where('kd_obat', 'LIKE', '%' . request('q') . '%')
+                    ->orWhere('nama_obat', 'LIKE', '%' . request('q') . '%')
+                    ->get();
+                $query->whereIn('kdobat', $kode);
+            })
             ->groupBy('kdobat', 'kdruang')
             ->paginate(request('per_page'));
 
