b2865cff596856273d2e7eb1a459d188a9de678f
duro06 <andi.meka.025@gmail.com>
2024-02-16 20:17:19 +0700

    simpan pemakaian sudah

--- app/Http/Controllers/Api/Simrs/Penunjang/Farmasinew/Ruangan/PemakaianRuanganController.php
+++ app/Http/Controllers/Api/Simrs/Penunjang/Farmasinew/Ruangan/PemakaianRuanganController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api\Simrs\Penunjang\Farmasinew\Ruangan;
 use App\Helpers\FormatingHelper;
 use App\Http\Controllers\Controller;
 use App\Models\Simrs\Penunjang\Farmasinew\Mobatnew;
+use App\Models\Simrs\Penunjang\Farmasinew\Ruangan\PemakaianH;
+use App\Models\Simrs\Penunjang\Farmasinew\Ruangan\PemakaianR;
 use App\Models\Simrs\Penunjang\Farmasinew\Stokreal;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -34,18 +36,119 @@ class PemakaianRuanganController extends Controller
 
     public function simpanpemaikaianruangan(Request $request)
     {
+        // return new JsonResponse($request->all());
+        try {
+            DB::beginTransaction();
 
-        return new JsonResponse($request->all());
+            if (!$request->nopemakaian) {
+                DB::connection('farmasi')->select('call pemakaianruangan(@nomor)');
+                $x = DB::connection('farmasi')->table('conter')->select('pemakaianruangan')->get();
+                $wew = $x[0]->pemakaianruangan;
+                $pemakaianruangan = FormatingHelper::nopemakaianruangan($wew, 'RUA-FAR');
+            } else {
+                $pemakaianruangan = $request->nopemakaian;
+            }
+            $user = FormatingHelper::session_user();
+            $kode = $user['kodesimrs'];
+            // header
+            $header = [
+                'nopemakaian' => $pemakaianruangan,
+                'tgl' => $request->tgl ? $request->tgl . date(' H:i:s') : date('Y-m-d H:i:s'),
+                'kdruang' => $request->kdruang,
+                'user' => $kode,
+            ];
 
-        DB::select('call pemakaianruangan(@nomor)');
-        $x = DB::table('conter')->select('pemakaianruangan')->get();
-        $wew = $x[0]->pemakaianruangan;
-        $pemakaianruangan = FormatingHelper::nopemakaianruangan($wew, 'RUA-FAR');
+            // rinci
+            $rinci = [];
+            foreach ($request->obats as $rin) {
+                if ((int)$rin['dipakai'] > 0) {
+                    $stok = Stokreal::where('kdobat', $rin['kdobat'])->where('kdruang', $request->kdruang)->orderBy('tglexp', 'ASC')->get();
+                    $index = 0;
+                    $dipakai = (int)$rin['dipakai'];
+                    while ($dipakai > 0) {
+                        $ada = (float)$stok[$index]->jumlah;
+                        if ($ada < $dipakai) {
+                            $temp = [
+                                'nopemakaian' => $pemakaianruangan,
+                                'kd_obat' => $rin['kdobat'],
+                                'nopenerimaan' => $stok[$index]->nopenerimaan,
+                                'nobatch' => $stok[$index]->nobatch,
+                                'jumlah' => $ada,
+                                'flag' => '',
+                            ];
+                            $rinci[] = $temp;
+                            $sisa = $dipakai - $ada;
+                            $index += 1;
+                            $dipakai = $sisa;
+                        } else {
+                            $temp = [
+                                'nopemakaian' => $pemakaianruangan,
+                                'kd_obat' => $rin['kdobat'],
+                                'nopenerimaan' => $stok[$index]->nopenerimaan,
+                                'nobatch' => $stok[$index]->nobatch,
+                                'jumlah' => $dipakai,
+                                'flag' => '',
+                            ];
+                            $rinci[] = $temp;
+                            $dipakai = 0;
+                            $sisa = $ada - $dipakai;
+                        }
+                    }
+                }
+            }
+            //simpan header
+            $simpahHead = PemakaianH::firstOrCreate(['nopemakaian' => $pemakaianruangan], $header);
+            // if (!$simpahHead) {
+            //     return new JsonResponse(['message' => 'Header Data Gagal Disimpan'], 410);
+            // }
+            // simpan rinci
+            $simpanRinci = PemakaianR::insert($rinci);
+            // update stok
+            $rinc = PemakaianR::where('nopemakaian', $pemakaianruangan)->where('flag', '')->get();
+            $st = [];
+            $ss = [];
+            foreach ($rinc as $rin) {
+                $stok = Stokreal::where('kdobat', $rin['kd_obat'])
+                    ->where('kdruang', $request->kdruang)
+                    ->where('nopenerimaan', $rin['nopenerimaan'])
+                    ->first();
+                $st[] = $stok;
+                if ($stok->jumlah > 0) {
+
+                    $sisa = $stok->jumlah - $rin['jumlah'];
+                    $ss[] = ['sisa' => $sisa, 'stok' => $stok->jumlah, 'dipakai' => $rin['jumlah']];
+                    $stok->jumlah = $sisa;
+                    $stok->save();
+                    $rin['flag'] = '1';
+                    $rin->save();
+                }
+            }
+            DB::commit();
+            return new JsonResponse([
+                'head' => $simpahHead,
+                'rinci' => $simpanRinci,
+                'message' => 'Pemakaian Disimpan, stok berkurang. '
+            ]);
+        } catch (\Exception $e) {
+            DB::rollback();
+            return new JsonResponse([
+                'message' => 'Data Gagal Disimpan...!!!',
+                'result' => $e,
+                'rinc' => $rinc,
+                'st' => $st,
+                'ss' => $ss,
+            ], 410);
+        }
     }
 
     public function selesaiPakai(Request $request)
     {
-
-        return new JsonResponse($request->all());
+        $data = PemakaianH::where('nopemakaian', $request->nopemakaian)->first();
+        $data->flag = '1';
+        $data->save();
+        return new JsonResponse([
+            'message' => 'Data sudah dikunci.',
+            'data' => $data
+        ]);
     }
 }
