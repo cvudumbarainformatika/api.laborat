22278620317022c803e49a703c7f5f549091788d
kentangkotak <ariezlnd69@gmail.com>
2024-03-21 20:47:16 +0700

    wew

--- app/Http/Controllers/Api/Simrs/Rajal/RmeRajalController.php
+++ app/Http/Controllers/Api/Simrs/Rajal/RmeRajalController.php
@@ -22,6 +22,35 @@ class RmeRajalController extends Controller
                     $a->with(['masterdiagnosa'])
                         ->orderBy('id', 'DESC');
                 },
+                'laborat' => function ($b) {
+                    $b->with(['pemeriksaanlab'])
+                        ->orderBy('id', 'DESC');
+                },
+                'pembacaanradiologi',
+                'apotekrajal' => function ($apotekrajal) {
+                    $apotekrajal->select('rs90.rs1', 'rs32.rs2 as obat', 'rs90.rs8 as jumlah')
+                        ->join('rs32', 'rs32.rs1', 'rs90.rs4');
+                },
+                'apotekrajalpolilalu' => function ($apotekrajalpolilalu) {
+                    $apotekrajalpolilalu->select('rs162.rs1', 'rs32.rs2 as obat', 'rs162.rs8 as jumlah')
+                        ->join('rs32', 'rs32.rs1', 'rs162.rs4');
+                },
+                'apotekracikanrajal' => function ($apotekracikanrajal) {
+                    $apotekracikanrajal->select('rs32.rs2 as obat', 'rs92.rs5 as jumlah')
+                        ->join('rs32', 'rs32.rs1', 'rs92.rs4');
+                },
+                'apotekracikanrajallalu' => function ($apotekracikanrajal) {
+                    $apotekracikanrajal->select('rs32.rs2 as obat', 'rs164.rs5 as jumlah')
+                        ->join('rs32', 'rs32.rs1', 'rs164.rs4');
+                },
+                'tindakan' => function ($tindakan) {
+                    $tindakan->select('rs73.rs1', 'rs30.rs2 as tindakan', 'rs73.rs20 as keterangan')
+                        ->join('rs30', 'rs30.rs1', 'rs73.rs4');
+                },
+                'planning' => function ($planning) {
+                    $planning
+                        ->where('rs4', 'not like', '%Pulang%');
+                },
             ]
         )->where('rs1', request('noreg'))
             ->get();
--- app/Models/Simrs/PemeriksaanRMkhusus/Polimata.php
+++ app/Models/Simrs/PemeriksaanRMkhusus/Polimata.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Simrs\PemeriksaanRMkhusus;
 
+use App\Models\Simrs\Pemeriksaanfisik\Pemeriksaanfisik;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,10 @@ class Polimata extends Model
     use HasFactory;
     protected $table = 'rs223';
     protected $guarded = ['id'];
+
+
+    public function relasivisus()
+    {
+        return $this->hasOne(Pemeriksaanfisik::class, 'rs236_id', 'id');
+    }
 }
