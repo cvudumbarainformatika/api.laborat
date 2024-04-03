244f5d9e7f360b10a7e4ca4fe0bdd8a546c3a1e5
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2024-04-03 23:18:28 +0700

    jhjkjhj

--- app/Models/Simrs/Pelayanan/Diagnosa/DiagnosaKebidanan.php
+++ app/Models/Simrs/Pelayanan/Diagnosa/DiagnosaKebidanan.php
@@ -6,7 +6,7 @@ use App\Models\Simrs\Pelayanan\Intervensikebidanan;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Diagnosakebidanan extends Model
+class DiagnosaKebidanan extends Model
 {
     use HasFactory;
     protected $table = 'diagnosakebidanan';
