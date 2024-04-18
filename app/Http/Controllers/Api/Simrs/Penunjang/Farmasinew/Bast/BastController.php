e8c3ec89eb934615a1f5fbe46e3e0805ca3376cd
duro06 <andi.meka.025@gmail.com>
2024-04-18 12:16:25 +0700

    sad

--- app/Http/Controllers/Api/Simrs/Penunjang/Farmasinew/Bast/BastController.php
+++ app/Http/Controllers/Api/Simrs/Penunjang/Farmasinew/Bast/BastController.php
@@ -4,46 +4,57 @@ namespace App\Http\Controllers\Api\Simrs\Penunjang\Farmasinew\Bast;
 
 use App\Helpers\FormatingHelper;
 use App\Http\Controllers\Controller;
-use App\Models\Simrs\Penunjang\Farmasinew\Bast\BasthederM;
-use App\Models\Simrs\Penunjang\Farmasinew\Bast\BastrinciM;
+use App\Models\Sigarang\KontrakPengerjaan;
 use App\Models\Simrs\Penunjang\Farmasinew\Pemesanan\PemesananHeder;
 use App\Models\Simrs\Penunjang\Farmasinew\Penerimaan\PenerimaanHeder;
-use App\Models\Simrs\Penunjang\Farmasinew\Penerimaan\PenerimaanRinci;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class BastController extends Controller
 {
-    public function dialogsp()
-    {
-        $sp = PemesananHeder::with(
-            [
-                'rinci',
-                'pihakketiga'
-            ]
-        )
-            ->where('flag', '1')
-            ->get();
-        return new JsonResponse($sp);
-    }
 
-    public function dialogpenerimaan()
+    public function perusahaan()
     {
-        $dialogpenerimaan = PenerimaanHeder::with(
-            [
-                'penerimaanrinci',
-                'penerimaanrinci.masterobat'
-            ]
-        )
-            ->where('kunci', '1')
-            ->where('nopemesanan', '!=', '')
+        // ambil perusahaan yang sedang belum bast
+        $raw = PenerimaanHeder::select('kdpbf')
+            ->where(function ($q) {
+                $q->whereNull('tgl_bast')
+                    ->orWhere('jumlah_bast', '<=', 0);
+            })
+            ->where('jenis_penerimaan', 'Pesanan')
             ->get();
-        return new JsonResponse($dialogpenerimaan);
-    }
+        // map kode perusahaan ke bentuk array, biar aman jika nanti ada append
+        $temp = collect($raw)->map(function ($y) {
+            return $y->kdpbf;
+        });
+        $data = KontrakPengerjaan::select('kodeperusahaan', 'namaperusahaan')->whereIn('kodeperusahaan', $temp)->distinct()->get();
 
-    public function simpanbast(Request $request)
+        return new JsonResponse($data);
+    }
+    public function pemesanan()
     {
+        $data = PemesananHeder::where('kdpbf', request('kdpbf'))->get();
+        return new JsonResponse($data);
+    }
+    public function penerimaan()
+    {
+        // return new JsonResponse(request()->all());
+        $data = PenerimaanHeder::where('kdpbf', request('kdpbf'))
+            ->where('nopemesanan', request('nopemesanan'))
+            ->where(function ($q) {
+                $q->whereNull('tgl_bast')
+                    ->orWhere('jumlah_bast', '<=', 0);
+            })
+            ->where('jenis_penerimaan', 'Pesanan')
+            ->with('penerimaanrinci.masterobat:kd_obat,nama_obat,satuan_b', 'faktur')
+            ->get();
+        return new JsonResponse($data);
+    }
+    public function simpan(Request $request)
+    {
+        return new JsonResponse($request->all());
+
         if ($request->nobast === '' || $request->nobast === null) {
             DB::connection('farmasi')->select('call nobast(@nomor)');
             $x = DB::connection('farmasi')->table('conter')->select('bast')->get();
@@ -52,46 +63,5 @@ class BastController extends Controller
         } else {
             $nobast = $request->nobast;
         }
-
-        $user = FormatingHelper::session_user();
-        $simpan = BasthederM::updateorcreate(
-            [
-                'nobast' => $nobast
-            ],
-            [
-                'tgl' => $request->tgl,
-                'nosp' => $request->nosep,
-                'penyedia' => $request->penyedia,
-                'user' => $user['kodesimrs']
-            ]
-        );
-        if (!$simpan) {
-            return new JsonResponse(['message' => 'BAST Gagal Disimpan...!!!'], 500);
-        }
-
-        $simpanr = BastrinciM::updateorcreate(
-            [
-                'nobast' => $nobast,
-                'nopenerimaan' => $request->nopenerimaan,
-                'kdobat' => $request->kdobat,
-                'jumlah' => $request->jumlah,
-                'harga' => $request->harga,
-                'diskon' => $request->diskon ?? 0,
-                'ppn' => $request->ppn,
-                'user' => $user['kodesimrs']
-            ]
-        );
-        if (!$simpanr) {
-            return new JsonResponse(['message' => 'Data Gagal Disimpan...!!!'], 500);
-        }
-        $penerimaan = PenerimaanRinci::where('nopenerimaan', $request->nopenerimaan)->count();
-        $bast = BastrinciM::where('nopenerimaan', $request->nopenerimaan)->count();
-        if ($penerimaan == $bast) {
-            $carihederpenerimaan = PenerimaanHeder::where('nopenerimaan', $request->nopenerimaan)->first();
-            $carihederpenerimaan->flag_bayar = '1';
-            $carihederpenerimaan->save();
-        }
-
-        return new JsonResponse(['message' => 'Data Berhasil Disimpan...!!!'], 200);
     }
 }
--- routes/v1/simrs/penunjang/farmasinew/bast.php
+++ routes/v1/simrs/penunjang/farmasinew/bast.php
@@ -9,9 +9,11 @@ Route::group([
     // 'middleware' => 'jwt.verify',
     'prefix' => 'simrs/farmasinew/bast'
 ], function () {
-    Route::get('/dialogsp', [BastController::class, 'dialogsp']);
-    Route::get('/dialogpenerimaan', [BastController::class, 'dialogpenerimaan']);
-    Route::post('/simpanbast', [BastController::class, 'simpanbast']);
+    Route::get('/perusahaan', [BastController::class, 'perusahaan']);
+    Route::get('/pemesanan', [BastController::class, 'pemesanan']);
+    Route::get('/penerimaan', [BastController::class, 'penerimaan']);
+
+    Route::post('/simpan', [BastController::class, 'simpan']);
 
     Route::get('/dialogsppajak', [PembebasanpajakController::class, 'dialogsppajak']);
 });
