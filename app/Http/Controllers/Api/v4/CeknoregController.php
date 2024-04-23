69933f74e2cf8a74cc3e78de5e42ad12c78e6bd4
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2024-04-23 19:19:57 +0700

    hhhheb

---
+++ app/Http/Controllers/Api/v4/CeknoregController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api\v4;
+
+use App\Events\NotifMessageEvent;
+use App\Helpers\FormatingHelper;
+use App\Http\Controllers\Controller;
+use App\Models\Simrs\Rajal\KunjunganPoli;
+use Carbon\Carbon;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CeknoregController extends Controller
+{
+  public function cek(Request $request)
+  {
+    $noreg=$request->noreg;
+    $cekx = KunjunganPoli::select('rs1', 'rs2', 'rs9', 'rs19')->where('rs1', $noreg)
+    ->with(['datasimpeg:id,nip,nik,nama,kelamin,foto,kdpegsimrs,kddpjp,ttdpegawai'])->first();
+    if (!$cekx) {
+      return new JsonResponse(['message' => 'invalid'], 500);
+    }
+    return new JsonResponse($cekx);
+  }
+}
