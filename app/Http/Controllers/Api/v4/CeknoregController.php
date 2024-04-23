fbbefa5fbc6006de71f42e80cc92ff8fc3a1d06d
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2024-04-23 19:39:29 +0700

    sfsdd

--- app/Http/Controllers/Api/v4/CeknoregController.php
+++ app/Http/Controllers/Api/v4/CeknoregController.php
@@ -16,6 +16,7 @@ class CeknoregController extends Controller
   public function cek(Request $request)
   {
     $noreg=$request->noreg;
+    return base64_decode($noreg);
     $cekx = KunjunganPoli::select('rs1', 'rs2', 'rs9', 'rs19')->where('rs1', $noreg)
     ->with(['datasimpeg:id,nip,nik,nama,kelamin,foto,kdpegsimrs,kddpjp,ttdpegawai'])->first();
     if (!$cekx) {
