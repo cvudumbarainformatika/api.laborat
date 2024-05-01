555db0ab09b5f2450a73b68214009378b7aef8e2
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2024-05-02 01:10:02 +0700

    asfasf

--- routes/v2/simrs/layananpoli/upload.php
+++ routes/v2/simrs/layananpoli/upload.php
@@ -10,6 +10,7 @@ Route::group([
     'prefix' => 'simrs/layananpoli/upload'
 ], function () {
     Route::post('/dokumen', [UploadController::class, 'store']);
+    Route::post('/deletedata', [UploadController::class, 'deletedata']);
     Route::get('/dokumenBy', [UploadController::class, 'dokumenBy']);
     Route::get('/master', [UploadController::class, 'master']);
 });
