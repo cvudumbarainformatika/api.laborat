4b0f92bfd6aeba0107cc4a1b57146caa746a6a97
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2024-06-13 15:03:21 +0700

    fgdfg

---
+++ routes/v1/simrs/penunjang/farmasinew/templateeresep.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Api\Simrs\Penunjang\Farmasinew\TemplateEresep\TemplateController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::group([
+    'middleware' => 'auth:api',
+    // 'middleware' => 'jwt.verify',
+    'prefix' => 'simrs/penunjang/farmasinew/templateeresep/'
+], function () {
+    Route::get('cariobat', [TemplateController::class, 'cariobat']);
+});
