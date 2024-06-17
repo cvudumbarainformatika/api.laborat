5e77ac64d78715e0e3c7ddb2ded2b2794ff45125
duro06 <andi.meka.meka.025@gmail.com>
2024-06-17 19:24:55 +0700

    simpan list

--- app/Http/Controllers/Api/Simrs/Penunjang/Farmasinew/Gudang/KonsinyasiController.php
+++ app/Http/Controllers/Api/Simrs/Penunjang/Farmasinew/Gudang/KonsinyasiController.php
@@ -6,6 +6,7 @@ use App\Helpers\FormatingHelper;
 use App\Http\Controllers\Controller;
 use App\Models\Simrs\Master\Mpihakketiga;
 use App\Models\Simrs\Penunjang\Farmasinew\Bast\BastKonsinyasi;
+use App\Models\Simrs\Penunjang\Farmasinew\Bast\DetailBastKonsinyasi;
 use App\Models\Simrs\Penunjang\Farmasinew\Depo\Resepkeluarrinci;
 use App\Models\Simrs\Penunjang\Farmasinew\Mobatnew;
 use App\Models\Simrs\Penunjang\Farmasinew\Obatoperasi\PersiapanOperasiDistribusi;
@@ -75,7 +76,7 @@ class KonsinyasiController extends Controller
             ->with([
                 'obat:kd_obat,nama_obat,satuan_k',
                 'header',
-                'resep:noresep,norm,noreg,dokter',
+                'resep:noresep,norm,noreg,dokter,tgl_permintaan',
                 'resep.dokter:kdpegsimrs,nama',
                 'resep.datapasien:rs1,rs2',
                 'rincian',
@@ -116,6 +117,7 @@ class KonsinyasiController extends Controller
             ->where('persiapan_operasi_rincis.jumlah_resep', '>', 0)
             ->where('persiapan_operasi_rincis.status_konsinyasi', '=', '1')
             ->whereIn('persiapan_operasi_distribusis.nopenerimaan', $pene)
+            ->whereNull('dibayar')
             ->get();
         $data = $resep;
         // $data['pene'] = $pene;
@@ -123,34 +125,112 @@ class KonsinyasiController extends Controller
     }
     public function simpanListKonsinyasi(Request $request)
     {
-        if (!$request->notranskonsi) {
-            $procedure = 'nokonsinyasi(@nomor)';
-            $colom = 'konsinyasi';
-            $lebel = '/TR/KONS';
-            DB::connection('farmasi')->select('call ' . $procedure);
-            $x = DB::connection('farmasi')->table('conter')->select($colom)->get();
-            $wew = $x[0]->$colom;
-            $notranskonsi = FormatingHelper::resep($wew, $lebel);
-        } else {
-            $notranskonsi = $request->notranskonsi;
+        if (count($request->items) <= 0) {
+            return new JsonResponse([
+                'message' => 'Tidak ada Data Barang',
+
+                $request->all()
+            ], 410);
         }
-        $head = BastKonsinyasi::updateOrCreate(
-            [
+
+        try {
+            DB::connection('farmasi')->beginTransaction();
+            $user = FormatingHelper::session_user();
+            if (!$request->notranskonsi) {
+                $procedure = 'nokonsinyasi(@nomor)';
+                $colom = 'konsinyasi';
+                $lebel = 'TR-KONS';
+                DB::connection('farmasi')->select('call ' . $procedure);
+                $x = DB::connection('farmasi')->table('conter')->select($colom)->get();
+                $wew = $x[0]->$colom;
+                $notranskonsi = FormatingHelper::resep($wew, $lebel);
+            } else {
+                $notranskonsi = $request->notranskonsi;
+            }
+            $create = date('Y-m-d H:i:s');
+            $head = BastKonsinyasi::updateOrCreate(
+                [
+                    'notranskonsi' => $notranskonsi,
+                    'kdpbf' => $request->penyedia,
+                ],
+                [
+                    'tgl_trans' => $request->tgl_trans,
+                    'jumlah_konsi' => $request->jumlah_konsi,
+                    'user_konsi' => $user['kodesimrs'],
+
+                ]
+            );
+            if (!$head) {
+                return new JsonResponse([
+                    'message' => 'Gagal Menyimpan Head Transaksi',
+                    'notranskonsi' => $notranskonsi,
+                    'user' => $user,
+                    'req' => $request->all()
+                ], 410);
+            }
+            // rinci
+            $rinci = [];
+            foreach ($request->items as $key) {
+                $temp = [
+                    'notranskonsi' => $notranskonsi,
+                    'nopermintaan' => $key['nopermintaan'],
+                    'nopenerimaan' => $key['nopenerimaan'],
+                    'kdobat' => $key['kdobat'],
+                    'tgl_pakai' => $key['tgl_pakai'],
+                    'tgl_penerimaan' => $key['tgl_penerimaan'],
+                    'dokter' => $key['dokter'],
+                    'noresep' => $key['noresep'],
+                    'noreg' => $key['noreg'],
+                    'norm' => $key['norm'],
+                    'jumlah' => $key['jumlah'],
+                    'harga' => $key['harga'],
+                    'diskon' => $key['diskon'],
+                    'ppn' => $key['ppn'],
+                    'diskon_rp' => $key['diskon_rp'],
+                    'ppn_rp' => $key['ppn_rp'],
+                    'harga_net' => $key['harga_net'],
+                    'subtotal' => $key['subtotal'],
+                    'created_at' => $create,
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ];
+                $rinci[] = $temp;
+            }
+
+            // hapus resep yang ada
+            $delRin = DetailBastKonsinyasi::where('notranskonsi', $notranskonsi)->delete();
+            // isi
+            $ins = DetailBastKonsinyasi::insert($rinci);
+
+            // update dibayar rinci permintaan operasi
+            $datanya = DetailBastKonsinyasi::where('notranskonsi', $notranskonsi)->get();
+            $rina = [];
+            foreach ($datanya as $det) {
+                $rinciPermintaanOP = PersiapanOperasiRinci::where('nopermintaan', $det->nopermintaan)
+                    ->where('noresep', $det->noresep)
+                    ->where('kd_obat', $det->kdobat)
+                    ->first();
+                $rina[] = $rinciPermintaanOP;
+                if ($rinciPermintaanOP) {
+                    $rinciPermintaanOP->update(['dibayar' => '1']);
+                }
+            }
+
+            DB::connection('farmasi')->commit();
+            return new JsonResponse([
+                'message' => 'List Sudah Disimpan',
                 'notranskonsi' => $notranskonsi,
-                'kdpbf' => $notranskonsi,
-            ],
-            [
-                'tgl_trans' => $notranskonsi,
-                'jumlah_konsi' => $notranskonsi,
-                'user_konsi' => $notranskonsi,
-
-            ]
-        );
-
-        return new JsonResponse([
-            'message' => 'List Sudah Disimpan',
-            'notranskonsi' => $notranskonsi,
-            $request->all()
-        ]);
+                'rinci' => $rinci,
+                'user' => $user,
+                'datanya' => $datanya,
+                'rina' => $rina,
+                'req' => $request->all()
+            ]);
+        } catch (\Exception $e) {
+            DB::connection('farmasi')->rollBack();
+            return new JsonResponse([
+                'message' => 'Ada Kesalahan, Semua Data Gagal Disimpan',
+                'result' => 'err ' . $e,
+            ], 410);
+        }
     }
 }
