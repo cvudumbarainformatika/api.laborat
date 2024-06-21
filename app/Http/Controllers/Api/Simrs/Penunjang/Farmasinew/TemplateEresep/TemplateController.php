9512d6ee02e941b6c05a55236d1f6bdc321a01dc
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2024-06-21 21:49:52 +0700

    asf

--- app/Http/Controllers/Api/Simrs/Penunjang/Farmasinew/TemplateEresep/TemplateController.php
+++ app/Http/Controllers/Api/Simrs/Penunjang/Farmasinew/TemplateEresep/TemplateController.php
@@ -51,13 +51,83 @@ class TemplateController extends Controller
         $user = auth()->user()->pegawai_id;
         $request->request->add(['pegawai_id' => $user]);
 
-        $cek = Templateresep::where('pegawai_id', $user)->where('nama', $request->nama)->where('kodedepo', $request->kodedepo)->count();
-        if ($cek > 0) {
-          return new JsonResponse(['message' => 'Maaf ... Template sudah ada, ganti nama'], 406);
+        $cek = Templateresep::where('pegawai_id', $user)->where('nama', $request->nama)->where('kodedepo', $request->kodedepo)->first();
+        if ($cek) {
+          // return new JsonResponse(['message' => 'Maaf ... Template sudah ada, ganti nama'], 406);
+          return self::updatetemplate($request, $cek);
         }
 
         // iF save not update
         return self::store($request);
+    }
+
+    public static function updatetemplate($request, $template)
+    {
+       try {
+        //code...
+        DB::beginTransaction();
+        TemplateResepRinci::where('template_id', $template->id)->delete();
+
+        $rinci = $request->items;
+        foreach ($rinci as $key => $value) {
+            $simpanRincian = [
+                'kodeobat'  => $value['kodeobat'],
+                'namaobat'  => $value['namaobat'],
+                'forkit'  => $value['forkit'],
+                'fornas'  => $value['fornas'],
+                'generik'  => $value['generik'],
+                'kandungan'  => $value['kandungan'],
+                'kekuatandosis'  => $value['kekuatandosis'],
+                'keterangan'  => $value['keterangan'],
+                'kode50'  => $value['kode50'],
+                'kode108'  => $value['kode108'],
+                'konsumsi'  => $value['konsumsi'],
+                'racikan'  => $value['racikan'] === true ? 1 : 0,
+                'satuan_kcl'  => $value['satuan_kcl'],
+                'signa'  => $value['signa'],
+                'jumlah_diminta'  => $value['jumlah_diminta'],
+                'tiperacikan'  => $value['tiperacikan'],
+                'tiperesep'  => $value['tiperesep'],
+                'template_id'  => $template->id
+            ];
+
+            $rincian = TemplateResepRinci::create($simpanRincian);
+            if ($value['racikan'] === true && $value['kodeobat'] === $rincian->kodeobat) {
+              // hapus dulu rincian racikan
+              TemplateResepRacikan::where('obat_id', $rincian->id)->delete();
+              foreach ($value['rincian'] as $k => $val) {
+                $racikan = [
+                  'obat_id'  => $rincian->id,
+                  'kodeobat'  => $val['kodeobat'],
+                  'namaobat'  => $val['namaobat'],
+                  'forkit'  => $val['forkit'],
+                  'fornas'  => $val['fornas'],
+                  'generik'  => $val['generik'],
+                  // 'kandungan'  => $val['kandungan'],
+                  'kekuatandosis'  => $val['kekuatandosis'],
+                  'keterangan'  => $val['keterangan'],
+                  'kode50'  => $val['kode50'],
+                  'kode108'  => $val['kode108'],
+                  // 'konsumsi'  => $val['konsumsi'],
+                  'satuan_kcl'  => $val['satuan_kcl'],
+                  // 'signa'  => $val['signa'],
+                  'jumlah_diminta'  => $val['jumlah_diminta'],
+                  'dosis'  => $val['dosis'],
+                  // 'tiperacikan'  => $val['tiperacikan'],
+                  // 'tiperesep'  => $val['tiperesep'],
+                ];
+
+                TemplateResepRacikan::create($racikan);
+              }
+            }
+        };
+        DB::commit();
+        return new JsonResponse($template->load(['rincian.rincian']), 200);
+
+       } catch (\Throwable $th) {
+          DB::rollback();
+          return new JsonResponse(['message' => 'Data Gagal Disimpan...!!!', 'result' => $th], 500);
+       }
     }
 
     public static function store($request)
