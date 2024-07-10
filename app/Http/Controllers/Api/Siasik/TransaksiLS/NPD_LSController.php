9d30768f25d10c446de4c088d7400fd5bbf1d2e6
pigartpigaro <vichoirul@gmail.com>
2024-07-10 10:07:59 +0700

    new siasik

---
+++ app/Http/Controllers/Api/Siasik/TransaksiLS/NPD_LSController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\Siasik\TransaksiLS;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class NPD_LSController extends Controller
+{
+    //
+}
--- app/Http/Controllers/Api/Siasik/TransaksiSaldo/TransSaldoController.php
+++ app/Http/Controllers/Api/Siasik/TransaksiSaldo/TransSaldoController.php
@@ -19,27 +19,38 @@ class TransSaldoController extends Controller
         return new JsonResponse( $saldo);
 
     }
+    public function tabelrek() {
+        $saldo = SaldoAwal_PPK::when(request('q'), function($x){
+        $x->where('rekening', request('q'));
+        })->get();
+        // ->where('noRek', request('rek'))
+
+        return new JsonResponse( $saldo);
+
+    }
     public function transSaldo(Request $request){
         try{
             DB::beginTransaction();
             if (!$request->has('id')){
-                SaldoAwal_PPK::firstOrCreate($request->only([
-                    'bulan',
-                    'tahun',
-                    'rekening',
-                    'nilaisaldo'
-                ]));
+                SaldoAwal_PPK::firstOrCreate([
+                    'noregister'=> self::buatnomor(),
+                    'tanggal' => $request->tanggal,
+                    'rekening' => $request->rekening,
+                    'namaRek' => $request->namaRek,
+                    'nilaisaldo' => $request->nilaisaldo
+                ]);
             } else {
                 $data = SaldoAwal_PPK::find($request->id);
-                $data->update($request->only([
-                    'bulan',
-                    'tahun',
-                    'rekening',
-                    'nilaisaldo'
-                ]));
+                $data->update([
+                    'noregister'=> self::buatnomor(),
+                    'tanggal' => $request->tanggal,
+                    'rekening' => $request->rekening,
+                    'namaRek' => $request->namaRek,
+                    'nilaisaldo' => $request->nilaisaldo
+                ]);
             }
             DB::commit();
-            return response()->json(['message' => 'Succes'], 200);
+            return response()->json(['message' =>'Berhasil Disimpan', 'succes'], 200);
         } catch (\Exception $e) {
             DB::rollBack();
             return response()->json(['message' => 'ada kesalahan', 'error' => $e], 500);
@@ -66,5 +77,41 @@ class TransSaldoController extends Controller
         return response()->json([
             'message' => 'Data sukses terhapus'
         ], 200);
+    }
+    public static function buatnomor(){
+        $huruf = ('SALDOAWAL-BLUD');
+        // $no = ('4.02.0.00.0.00.01.0000');
+        date_default_timezone_set('Asia/Jakarta');
+        // $tgl = date('Y/m/d');
+        $thn = date('Y');
+        $rom = array('','I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII');
+        // $time = date('mis');
+        // $nomer=Transaksi::latest();
+        $cek = SaldoAwal_PPK::count();
+        if ($cek == null){
+            $urut = "0001";
+            $sambung = $urut.'/'.strtoupper($huruf).'/'.$rom[date('n')].'/'.$thn;
+        }
+        else{
+            $ambil=SaldoAwal_PPK::all()->last();
+            $urut = (int)substr($ambil->noregister, 1, 3) + 1;
+            //cara menyambungkan antara tgl dn kata dihubungkan tnda .
+            // $urut = "000" . $urut;
+            if(strlen($urut) == 1){
+                $urut = "000" . $urut;
+            }
+            else if(strlen($urut) == 2){
+                $urut = "00" . $urut;
+            }
+            else if(strlen($urut) == 3){
+                $urut = "0" . $urut;
+            }
+            else {
+                $urut = (int)$urut;
+            }
+            $sambung = $urut.'/'.strtoupper($huruf).'/'.$rom[date('n')].'/'.$thn;
+        }
+
+        return $sambung;
     }
 }
