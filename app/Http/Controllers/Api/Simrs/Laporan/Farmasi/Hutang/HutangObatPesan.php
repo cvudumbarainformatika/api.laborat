b60d0cbf89436f8972ac3ce3ae8f08ab5764a888
kentangkotak <ariezln69@gmail.com>
2024-08-07 09:46:11 +0700

    wew

--- app/Http/Controllers/Api/Simrs/Laporan/Farmasi/Hutang/HutangObatPesan.php
+++ app/Http/Controllers/Api/Simrs/Laporan/Farmasi/Hutang/HutangObatPesan.php
@@ -22,7 +22,6 @@ class HutangObatPesan extends Controller
             ]
         )
         ->where('jenis_penerimaan','Pesanan')
-        ->limit(20)
         ->get();
         return new JsonResponse($data);
     }
