6b28a04d8eef8ac2f97e7478f5772a04faead20b
kentangkotak <ariezlnd69@gmail.com>
2024-09-04 14:56:00 +0700

    wew

---
+++ app/Http/Controllers/Api/Simrs/Igd/TriageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api\Simrs\Igd;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TriageController extends Controller
+{
+    public function simpantriage()
+    {
+        return 'wew';
+    }
+}
