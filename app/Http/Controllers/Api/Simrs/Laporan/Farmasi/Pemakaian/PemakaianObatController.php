9a40a59f5d4e4e70d56b00d39a6d96892e15f9f5
duro06 <andi.meka.025@gmail.com>
2024-09-10 08:35:04 +0700

    laporan pemakaian akhir bulan lalu luput

--- app/Http/Controllers/Api/Simrs/Laporan/Farmasi/Pemakaian/PemakaianObatController.php
+++ app/Http/Controllers/Api/Simrs/Laporan/Farmasi/Pemakaian/PemakaianObatController.php
@@ -12,14 +12,15 @@ use Illuminate\Support\Facades\DB;
 
 class PemakaianObatController extends Controller
 {
-    function getAllPemakaianObat(){
-        
+    function getAllPemakaianObat()
+    {
+
         $dateAwal = Carbon::parse(request('from'));
         $dateAkhir = Carbon::parse(request('to'));
         $blnLaluAwal = $dateAwal->subMonth()->format('Y-m-d');
         $blnLaluAkhir = $dateAkhir->subMonth()->format('Y-m-d');
-        $gudangdepo=['Gd-03010100', 'Gd-03010101', 'Gd-05010100', 'Gd-04010102', 'Gd-04010103', 'Gd-05010101', 'Gd-02010104']; //  
-        $obat=Mobatnew::select(
+        $gudangdepo = ['Gd-03010100', 'Gd-03010101', 'Gd-05010100', 'Gd-04010102', 'Gd-04010103', 'Gd-05010101', 'Gd-02010104']; //
+        $obat = Mobatnew::select(
             'kd_obat',
             'nama_obat',
             'bentuk_sediaan',
@@ -31,91 +32,91 @@ class PemakaianObatController extends Controller
             'status_kronis',
             'kelompok_psikotropika',
             'kode108',
-            )
-        ->with([
-            'kodebelanja:kode,uraian,uraianB',
-            'saldoawal'=>function($sal) use($blnLaluAkhir,$gudangdepo){
-                $sal->select(
-                    'kdobat',
-                    DB::raw('sum(jumlah) as jumlah')
-                )
-                ->where('tglopname','LIKE','%'.$blnLaluAkhir.'%')
-                ->whereIn('kdruang',$gudangdepo)
-                ->groupBy('tglopname','kdobat');
-            },
-            'penerimaanrinci'=>function($ter){
-                $ter->select(
-                    'penerimaan_r.kdobat',
-                    DB::raw('sum(penerimaan_r.jml_terima_k) as jumlah')
-                )
-                ->leftJoin('penerimaan_h','penerimaan_h.nopenerimaan','=','penerimaan_r.nopenerimaan')
-                ->whereBetween('tglpenerimaan',[request('from').' 00:00:00',request('to').' 23:59:59'])
-                ->groupBy('penerimaan_r.kdobat');
-            },
-            'mutasikeluar'=>function($mut){
-                $mut->select(
-                    'mutasi_gudangdepo.kd_obat',
-                    DB::raw('sum(mutasi_gudangdepo.jml) as jumlah'),
-                    DB::raw('sum(mutasi_gudangdepo.jml * mutasi_gudangdepo.harga) as subtotal'),
+        )
+            ->with([
+                'kodebelanja:kode,uraian,uraianB',
+                'saldoawal' => function ($sal) use ($blnLaluAkhir, $gudangdepo) {
+                    $sal->select(
+                        'kdobat',
+                        DB::raw('sum(jumlah) as jumlah')
                     )
-                ->leftJoin('permintaan_h','permintaan_h.no_permintaan','=','mutasi_gudangdepo.no_permintaan')
-                ->whereBetween('permintaan_h.tgl_kirim_depo',[request('from').' 00:00:00',request('to').' 23:59:59'])
-                ->where('permintaan_h.dari','LIKE','%R-%');
-            },
-            
-            'resepkeluar'=>function($kel){
-                $kel->select(
-                    'resep_keluar_r.kdobat',
-                    'resep_keluar_h.sistembayar',
-                    DB::raw('sum(resep_keluar_r.jumlah) as jumlah'),
-                    DB::raw('sum(resep_keluar_r.jumlah * resep_keluar_r.harga_jual) as subtotal'),
-                )
-                ->leftJoin('resep_keluar_h','resep_keluar_h.noresep','=','resep_keluar_r.noresep')
-                ->whereBetween('resep_keluar_h.tgl_selesai',[request('from').' 00:00:00',request('to').' 23:59:59'])
-                ->groupBy('resep_keluar_h.sistembayar','resep_keluar_r.kdobat');
-                
-            },
-            
-            'resepkeluarracikan'=>function($kel){
-                $kel->select(
-                    'resep_keluar_racikan_r.kdobat',
-                    'resep_keluar_h.sistembayar',
-                    DB::raw('sum(resep_keluar_racikan_r.jumlah) as jumlah'),
-                    DB::raw('sum(resep_keluar_racikan_r.jumlah * resep_keluar_racikan_r.harga_jual) as subtotal'),
-                )
-                ->leftJoin('resep_keluar_h','resep_keluar_h.noresep','=','resep_keluar_racikan_r.noresep')
-                ->whereBetween('resep_keluar_h.tgl_selesai',[request('from').' 00:00:00',request('to').' 23:59:59'])
-                ->groupBy('resep_keluar_h.sistembayar','resep_keluar_racikan_r.kdobat');                
-            }, 
-            'returpenjualan'=>function($kel){
-                $kel->select(
-                    'retur_penjualan_r.kdobat',
-                    'resep_keluar_h.sistembayar',
-                    DB::raw('sum(retur_penjualan_r.jumlah_keluar) as jumlah'),
-                    DB::raw('sum(retur_penjualan_r.jumlah_keluar * retur_penjualan_r.harga_jual) as subtotal'),
-                )
-                ->leftJoin('retur_penjualan_h','retur_penjualan_h.noretur','=','retur_penjualan_r.noretur')
-                ->leftJoin('resep_keluar_h','resep_keluar_h.noresep','=','retur_penjualan_h.noresep')
-                ->whereBetween('retur_penjualan_h.tgl_retur',[request('from').' 00:00:00',request('to').' 23:59:59'])
-                ->groupBy('resep_keluar_h.sistembayar','retur_penjualan_r.kdobat');                
-            }
-        ])
-        ->get();
+                        ->where('tglopname', 'LIKE', '%' . $blnLaluAkhir . '%')
+                        ->whereIn('kdruang', $gudangdepo)
+                        ->groupBy('tglopname', 'kdobat');
+                },
+                'penerimaanrinci' => function ($ter) {
+                    $ter->select(
+                        'penerimaan_r.kdobat',
+                        DB::raw('sum(penerimaan_r.jml_terima_k) as jumlah')
+                    )
+                        ->leftJoin('penerimaan_h', 'penerimaan_h.nopenerimaan', '=', 'penerimaan_r.nopenerimaan')
+                        ->whereBetween('tglpenerimaan', [request('from') . ' 00:00:00', request('to') . ' 23:59:59'])
+                        ->groupBy('penerimaan_r.kdobat');
+                },
+                'mutasikeluar' => function ($mut) {
+                    $mut->select(
+                        'mutasi_gudangdepo.kd_obat',
+                        DB::raw('sum(mutasi_gudangdepo.jml) as jumlah'),
+                        DB::raw('sum(mutasi_gudangdepo.jml * mutasi_gudangdepo.harga) as subtotal'),
+                    )
+                        ->leftJoin('permintaan_h', 'permintaan_h.no_permintaan', '=', 'mutasi_gudangdepo.no_permintaan')
+                        ->whereBetween('permintaan_h.tgl_kirim_depo', [request('from') . ' 00:00:00', request('to') . ' 23:59:59'])
+                        ->where('permintaan_h.dari', 'LIKE', '%R-%');
+                },
+
+                'resepkeluar' => function ($kel) {
+                    $kel->select(
+                        'resep_keluar_r.kdobat',
+                        'resep_keluar_h.sistembayar',
+                        DB::raw('sum(resep_keluar_r.jumlah) as jumlah'),
+                        DB::raw('sum(resep_keluar_r.jumlah * resep_keluar_r.harga_jual) as subtotal'),
+                    )
+                        ->leftJoin('resep_keluar_h', 'resep_keluar_h.noresep', '=', 'resep_keluar_r.noresep')
+                        ->whereBetween('resep_keluar_h.tgl_selesai', [request('from') . ' 00:00:00', request('to') . ' 23:59:59'])
+                        ->groupBy('resep_keluar_h.sistembayar', 'resep_keluar_r.kdobat');
+                },
+
+                'resepkeluarracikan' => function ($kel) {
+                    $kel->select(
+                        'resep_keluar_racikan_r.kdobat',
+                        'resep_keluar_h.sistembayar',
+                        DB::raw('sum(resep_keluar_racikan_r.jumlah) as jumlah'),
+                        DB::raw('sum(resep_keluar_racikan_r.jumlah * resep_keluar_racikan_r.harga_jual) as subtotal'),
+                    )
+                        ->leftJoin('resep_keluar_h', 'resep_keluar_h.noresep', '=', 'resep_keluar_racikan_r.noresep')
+                        ->whereBetween('resep_keluar_h.tgl_selesai', [request('from') . ' 00:00:00', request('to') . ' 23:59:59'])
+                        ->groupBy('resep_keluar_h.sistembayar', 'resep_keluar_racikan_r.kdobat');
+                },
+                'returpenjualan' => function ($kel) {
+                    $kel->select(
+                        'retur_penjualan_r.kdobat',
+                        'resep_keluar_h.sistembayar',
+                        DB::raw('sum(retur_penjualan_r.jumlah_keluar) as jumlah'),
+                        DB::raw('sum(retur_penjualan_r.jumlah_keluar * retur_penjualan_r.harga_jual) as subtotal'),
+                    )
+                        ->leftJoin('retur_penjualan_h', 'retur_penjualan_h.noretur', '=', 'retur_penjualan_r.noretur')
+                        ->leftJoin('resep_keluar_h', 'resep_keluar_h.noresep', '=', 'retur_penjualan_h.noresep')
+                        ->whereBetween('retur_penjualan_h.tgl_retur', [request('from') . ' 00:00:00', request('to') . ' 23:59:59'])
+                        ->groupBy('resep_keluar_h.sistembayar', 'retur_penjualan_r.kdobat');
+                }
+            ])
+            ->get();
         $obat->append('harga');
         return new JsonResponse([
-            'data'=>$obat,
-            'req'=>request()->all(),
+            'data' => $obat,
+            'req' => request()->all(),
             // 'blnLaluAkhir'=>$blnLaluAkhir
         ]);
     }
-    function getPemakaianObat(){
-        
+    function getPemakaianObat()
+    {
+
         $dateAwal = Carbon::parse(request('from'));
         $dateAkhir = Carbon::parse(request('to'));
         $blnLaluAwal = $dateAwal->subMonth()->format('Y-m-d');
-        $blnLaluAkhir = $dateAkhir->subMonth()->format('Y-m-d');
-        $gudangdepo=['Gd-03010100', 'Gd-03010101', 'Gd-05010100', 'Gd-04010102', 'Gd-04010103', 'Gd-05010101', 'Gd-02010104']; //  
-        $obat=Mobatnew::select(
+        $blnLaluAkhir = $dateAkhir->subMonth()->format('Y-m-t');
+        $gudangdepo = ['Gd-03010100', 'Gd-03010101', 'Gd-05010100', 'Gd-04010102', 'Gd-04010103', 'Gd-05010101', 'Gd-02010104']; //
+        $obat = Mobatnew::select(
             'kd_obat',
             'nama_obat',
             'bentuk_sediaan',
@@ -127,90 +128,90 @@ class PemakaianObatController extends Controller
             'status_kronis',
             'kelompok_psikotropika',
             'kode108',
-            )
-        ->with([
-            'kodebelanja:kode,uraian,uraianB',
-            'saldoawal'=>function($sal) use($blnLaluAkhir,$gudangdepo){
-                $sal->select(
-                    'kdobat',
-                    DB::raw('sum(jumlah) as jumlah')
-                )
-                ->where('tglopname','LIKE','%'.$blnLaluAkhir.'%')
-                ->whereIn('kdruang',$gudangdepo)
-                ->groupBy('kdobat');
-            },
-            'penerimaanrinci'=>function($ter){
-                $ter->select(
-                    'penerimaan_r.kdobat',
-                    DB::raw('sum(penerimaan_r.jml_terima_k) as jumlah')
-                )
-                ->leftJoin('penerimaan_h','penerimaan_h.nopenerimaan','=','penerimaan_r.nopenerimaan')
-                ->whereBetween('tglpenerimaan',[request('from').' 00:00:00',request('to').' 23:59:59'])
-                ->groupBy('penerimaan_r.kdobat');
-            },
-            'mutasikeluar'=>function($mut){
-                $mut->select(
-                    'mutasi_gudangdepo.kd_obat',
-                    DB::raw('sum(mutasi_gudangdepo.jml) as jumlah'),
-                    DB::raw('sum(mutasi_gudangdepo.jml * mutasi_gudangdepo.harga) as subtotal'),
+        )
+            ->with([
+                'kodebelanja:kode,uraian,uraianB',
+                'saldoawal' => function ($sal) use ($blnLaluAkhir, $gudangdepo) {
+                    $sal->select(
+                        'kdobat',
+                        DB::raw('sum(jumlah) as jumlah')
                     )
-                ->leftJoin('permintaan_h','permintaan_h.no_permintaan','=','mutasi_gudangdepo.no_permintaan')
-                ->whereBetween('permintaan_h.tgl_kirim_depo',[request('from').' 00:00:00',request('to').' 23:59:59'])
-                ->where('permintaan_h.dari','LIKE','%R-%');
-            },
-            
-            'resepkeluar'=>function($kel){
-                $kel->select(
-                    'resep_keluar_r.kdobat',
-                    'resep_keluar_h.sistembayar',
-                    DB::raw('sum(resep_keluar_r.jumlah) as jumlah'),
-                    DB::raw('sum(resep_keluar_r.jumlah * resep_keluar_r.harga_jual) as subtotal'),
-                )
-                ->leftJoin('resep_keluar_h','resep_keluar_h.noresep','=','resep_keluar_r.noresep')
-                ->whereBetween('resep_keluar_h.tgl_selesai',[request('from').' 00:00:00',request('to').' 23:59:59'])
-                ->groupBy('resep_keluar_h.sistembayar','resep_keluar_r.kdobat');
-                
-            },
-            
-            'resepkeluarracikan'=>function($kel){
-                $kel->select(
-                    'resep_keluar_racikan_r.kdobat',
-                    'resep_keluar_h.sistembayar',
-                    DB::raw('sum(resep_keluar_racikan_r.jumlah) as jumlah'),
-                    DB::raw('sum(resep_keluar_racikan_r.jumlah * resep_keluar_racikan_r.harga_jual) as subtotal'),
-                )
-                ->leftJoin('resep_keluar_h','resep_keluar_h.noresep','=','resep_keluar_racikan_r.noresep')
-                ->whereBetween('resep_keluar_h.tgl_selesai',[request('from').' 00:00:00',request('to').' 23:59:59'])
-                ->groupBy('resep_keluar_h.sistembayar','resep_keluar_racikan_r.kdobat');                
-            }, 
-            'returpenjualan'=>function($kel){
-                $kel->select(
-                    'retur_penjualan_r.kdobat',
-                    'resep_keluar_h.sistembayar',
-                    DB::raw('sum(retur_penjualan_r.jumlah_keluar) as jumlah'),
-                    DB::raw('sum(retur_penjualan_r.jumlah_keluar * retur_penjualan_r.harga_jual) as subtotal'),
-                )
-                ->leftJoin('retur_penjualan_h','retur_penjualan_h.noretur','=','retur_penjualan_r.noretur')
-                ->leftJoin('resep_keluar_h','resep_keluar_h.noresep','=','retur_penjualan_h.noresep')
-                ->whereBetween('retur_penjualan_h.tgl_retur',[request('from').' 00:00:00',request('to').' 23:59:59'])
-                ->groupBy('resep_keluar_h.sistembayar','retur_penjualan_r.kdobat');                
-            }
-        ])
-        ->where('nama_obat','LIKE','%'.request('q').'%')
-        ->orWhere('kd_obat','LIKE','%'.request('q').'%')
-        ->paginate(request('per_page'));
+                        ->where('tglopname', 'LIKE', '%' . $blnLaluAkhir . '%')
+                        ->whereIn('kdruang', $gudangdepo)
+                        ->groupBy('kdobat');
+                },
+                'penerimaanrinci' => function ($ter) {
+                    $ter->select(
+                        'penerimaan_r.kdobat',
+                        DB::raw('sum(penerimaan_r.jml_terima_k) as jumlah')
+                    )
+                        ->leftJoin('penerimaan_h', 'penerimaan_h.nopenerimaan', '=', 'penerimaan_r.nopenerimaan')
+                        ->whereBetween('tglpenerimaan', [request('from') . ' 00:00:00', request('to') . ' 23:59:59'])
+                        ->groupBy('penerimaan_r.kdobat');
+                },
+                'mutasikeluar' => function ($mut) {
+                    $mut->select(
+                        'mutasi_gudangdepo.kd_obat',
+                        DB::raw('sum(mutasi_gudangdepo.jml) as jumlah'),
+                        DB::raw('sum(mutasi_gudangdepo.jml * mutasi_gudangdepo.harga) as subtotal'),
+                    )
+                        ->leftJoin('permintaan_h', 'permintaan_h.no_permintaan', '=', 'mutasi_gudangdepo.no_permintaan')
+                        ->whereBetween('permintaan_h.tgl_kirim_depo', [request('from') . ' 00:00:00', request('to') . ' 23:59:59'])
+                        ->where('permintaan_h.dari', 'LIKE', '%R-%');
+                },
+
+                'resepkeluar' => function ($kel) {
+                    $kel->select(
+                        'resep_keluar_r.kdobat',
+                        'resep_keluar_h.sistembayar',
+                        DB::raw('sum(resep_keluar_r.jumlah) as jumlah'),
+                        DB::raw('sum(resep_keluar_r.jumlah * resep_keluar_r.harga_jual) as subtotal'),
+                    )
+                        ->leftJoin('resep_keluar_h', 'resep_keluar_h.noresep', '=', 'resep_keluar_r.noresep')
+                        ->whereBetween('resep_keluar_h.tgl_selesai', [request('from') . ' 00:00:00', request('to') . ' 23:59:59'])
+                        ->groupBy('resep_keluar_h.sistembayar', 'resep_keluar_r.kdobat');
+                },
+
+                'resepkeluarracikan' => function ($kel) {
+                    $kel->select(
+                        'resep_keluar_racikan_r.kdobat',
+                        'resep_keluar_h.sistembayar',
+                        DB::raw('sum(resep_keluar_racikan_r.jumlah) as jumlah'),
+                        DB::raw('sum(resep_keluar_racikan_r.jumlah * resep_keluar_racikan_r.harga_jual) as subtotal'),
+                    )
+                        ->leftJoin('resep_keluar_h', 'resep_keluar_h.noresep', '=', 'resep_keluar_racikan_r.noresep')
+                        ->whereBetween('resep_keluar_h.tgl_selesai', [request('from') . ' 00:00:00', request('to') . ' 23:59:59'])
+                        ->groupBy('resep_keluar_h.sistembayar', 'resep_keluar_racikan_r.kdobat');
+                },
+                'returpenjualan' => function ($kel) {
+                    $kel->select(
+                        'retur_penjualan_r.kdobat',
+                        'resep_keluar_h.sistembayar',
+                        DB::raw('sum(retur_penjualan_r.jumlah_keluar) as jumlah'),
+                        DB::raw('sum(retur_penjualan_r.jumlah_keluar * retur_penjualan_r.harga_jual) as subtotal'),
+                    )
+                        ->leftJoin('retur_penjualan_h', 'retur_penjualan_h.noretur', '=', 'retur_penjualan_r.noretur')
+                        ->leftJoin('resep_keluar_h', 'resep_keluar_h.noresep', '=', 'retur_penjualan_h.noresep')
+                        ->whereBetween('retur_penjualan_h.tgl_retur', [request('from') . ' 00:00:00', request('to') . ' 23:59:59'])
+                        ->groupBy('resep_keluar_h.sistembayar', 'retur_penjualan_r.kdobat');
+                }
+            ])
+            ->where('nama_obat', 'LIKE', '%' . request('q') . '%')
+            ->orWhere('kd_obat', 'LIKE', '%' . request('q') . '%')
+            ->paginate(request('per_page'));
         $obat->append('harga');
-        $data=collect($obat)['data'];
-        $meta=collect($obat)->except('data');
+        $data = collect($obat)['data'];
+        $meta = collect($obat)->except('data');
         return new JsonResponse([
-            'data'=>$data,
-            'meta'=>$meta,
-            'req'=>request()->all(),
-            'blnLaluAkhir'=>$blnLaluAkhir
+            'data' => $data,
+            'meta' => $meta,
+            'req' => request()->all(),
+            'blnLaluAkhir' => $blnLaluAkhir
         ]);
     }
-    public function getSistemBayar(){
-        $data=SistemBayar::get();
+    public function getSistemBayar()
+    {
+        $data = SistemBayar::get();
         return new JsonResponse($data);
     }
 }
