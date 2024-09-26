74823b378860ee2ecaddae0a1d20e07a76e4be2a
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2024-09-26 15:28:50 +0700

    anamnesis

--- app/Models/Simrs/Anamnesis/Anamnesis.php
+++ app/Models/Simrs/Anamnesis/Anamnesis.php
@@ -3,18 +3,33 @@
 namespace App\Models\Simrs\Anamnesis;
 
 use App\Models\Pegawai\Mpegawaisimpeg;
+use App\Models\Simpeg\Petugas;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Anamnesis extends Model
 {
     use HasFactory;
+    protected $connection = 'mysql';
     protected $table = 'rs209';
     protected $guarded = ['id'];
+    protected $casts = [
+        'riwayatalergi' => 'array',
+      ];
 
 
     public function datasimpeg()
     {
         return  $this->hasOne(Mpegawaisimpeg::class, 'kdpegsimrs', 'user');
+    }
+
+    public function petugas()
+    {
+       return $this->hasOne(Petugas::class, 'kdpegsimrs','user');
+    }
+
+    public function keluhannyeri()
+    {
+        return $this->hasOne(KeluhanNyeri::class, 'rs209_id', 'id');
     }
 }
