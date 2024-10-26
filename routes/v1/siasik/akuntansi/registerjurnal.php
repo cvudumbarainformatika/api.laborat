e6b99abc0ec2b0ef9b0c28166f84181816e8794b
pigartpigaro <vichoirul@gmail.com>
2024-10-27 03:30:51 +0700

    new lapo

--- routes/v1/siasik/akuntansi/registerjurnal.php
+++ routes/v1/siasik/akuntansi/registerjurnal.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Api\Siasik\Akuntansi\JurnalumumController;
 use Illuminate\Support\Facades\Route;
 
 Route::group([
-    // 'middleware' => 'auth:api',
+    'middleware' => 'auth:api',
     'prefix' => 'akuntansi/registerjurnal'
 ], function () {
     Route::get('/regjurnal', [RegJurnalController::class, 'listjurnal']);
