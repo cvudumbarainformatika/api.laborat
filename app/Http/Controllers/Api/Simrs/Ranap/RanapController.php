df157a2e4549e202d7a44b5a35e6d26f28867ac9
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2024-11-07 08:41:15 +0700

    rev: kok sek putra ?

--- app/Http/Controllers/Api/Simrs/Ranap/RanapController.php
+++ app/Http/Controllers/Api/Simrs/Ranap/RanapController.php
@@ -16,7 +16,7 @@ class RanapController extends Controller
 {
     public function kunjunganpasien()
     {
-        // coba
+        // coba lagi
         // return request()->all();
         $dokter = request('kddokter');
 
