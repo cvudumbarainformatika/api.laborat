b10a18d7cb3ea0804a5f52dfb90a5b3b4ab7b372
duro06 <andi.meka.025@gmail.com>
2024-12-17 13:43:25 +0700

    rev : old url laporan

--- routes/v1/simrs/laporan/farmasi/pemakaian.php
+++ routes/v1/simrs/laporan/farmasi/pemakaian.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\Simrs\Laporan\Farmasi\Pemakaian\PemakaianObatController;
+use App\Http\Controllers\Api\Simrs\Laporan\Farmasi\Persediaan\PersediaanFiFoController;
 use Illuminate\Support\Facades\Route;
+
 Route::group([
     'middleware' => 'auth:api',
     // 'middleware' => 'jwt.verify',
@@ -10,4 +12,9 @@ Route::group([
     Route::get('/get-pemakaian', [PemakaianObatController::class, 'getPemakaianObat']);
     Route::get('/get-all-pemakaian', [PemakaianObatController::class, 'getAllPemakaianObat']);
     Route::get('/get-sistembayar', [PemakaianObatController::class, 'getSistemBayar']);
+
+    // url lama sebelum update fron end
+
+    Route::get('/get-persediaan', [PersediaanFiFoController::class, 'getPersediaan']);
+    Route::get('/get-mutasi', [PersediaanFiFoController::class, 'getMutasi']);
 });
