ddf028da5acc526038c387db35fba754dab25832
kentangkotak <ariezlnd69@gmail.com>
2025-01-06 10:58:19 +0700

    ref : surat kematian

--- app/Models/Simrs/Planing/Planing_Igd_Pulang.php
+++ app/Models/Simrs/Planing/Planing_Igd_Pulang.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Simrs\Planing;
 
+use App\Models\Simpeg\Petugas;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,9 @@ class Planing_Igd_Pulang extends Model
     use HasFactory;
     protected $table = 'plann_igd_pulang';
     protected $guarded = ['id'];
+
+    public function dokterpenangungjawabpulang()
+    {
+        return $this->hasOne(Petugas::class, 'kdpegsimrs', 'user_dokter');
+    }
 }
