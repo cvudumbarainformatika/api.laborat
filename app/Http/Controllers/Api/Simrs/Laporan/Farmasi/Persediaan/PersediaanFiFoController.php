5a3606ac6fcf6a235666dfe4a0f4609a0b4b563b
duro06 <andi.meka.025@gmail.com>
2025-01-14 14:13:17 +0700

    rev : mutasi

--- app/Http/Controllers/Api/Simrs/Laporan/Farmasi/Persediaan/PersediaanFiFoController.php
+++ app/Http/Controllers/Api/Simrs/Laporan/Farmasi/Persediaan/PersediaanFiFoController.php
@@ -423,7 +423,10 @@ class PersediaanFiFoController extends Controller
                         ->havingRaw('jumlah > 0')
                         ->where(function ($query) {
                             $query->where('permintaan_h.dari', 'LIKE', 'R-%')
-                                ->orWhereIn('permintaan_h.dari', ['Gd-04010102', 'Gd-04010103', 'Gd-05010101', 'Gd-02010104']);
+                                ->orWhere(function ($f) {
+                                    $f->whereIn('permintaan_h.dari', ['Gd-04010102', 'Gd-04010103', 'Gd-05010101', 'Gd-02010104'])
+                                        ->where('permintaan_h.tujuan', 'Gd-03010100');
+                                });
                         })
                         // ->where('permintaan_h.dari', 'LIKE', 'R-%')
                         ->where('permintaan_h.tgl_kirim_depo', 'LIKE', request('tahun') . '-' . request('bulan') . '%')
