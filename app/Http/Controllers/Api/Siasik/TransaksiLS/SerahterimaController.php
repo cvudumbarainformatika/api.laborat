44556687da4cd939a235eb94902939ae3ee7425d
pigartpigaro <vichoirul@gmail.com>
2025-02-19 19:05:00 +0700

    wip: Serah Terima Siasik dan update manual user pencairan

---
+++ app/Http/Controllers/Api/Siasik/TransaksiLS/SerahterimaController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers\Api\Siasik\TransaksiLS;
+
+use App\Helpers\FormatingHelper;
+use App\Http\Controllers\Controller;
+use App\Models\Siasik\TransaksiLS\NpdLS_heder;
+use App\Models\Siasik\TransaksiLS\Serahterima_header;
+use App\Models\Sigarang\KontrakPengerjaan;
+use App\Models\Sigarang\Pegawai;
+use Carbon\Carbon;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use PhpParser\Node\Stmt\Return_;
+
+class SerahterimaController extends Controller
+{
+    public function getkontrak(){
+        $tahun=Carbon::createFromFormat('Y', request('tahun'))->format('Y');
+        $data = KontrakPengerjaan::where('kunci', '!=', '')
+        ->whereBetween('tgltrans', [$tahun.'-01-01', $tahun.'-12-31'])
+        ->when(request('q'), function($q){
+            $q->where('nokontrak', 'LIKE', '%' . request('q') . '%')
+                ->orWhere('namaperusahaan', 'LIKE', '%' . request('q') . '%')
+                ->orWhere('namapptk', 'LIKE', '%' . request('q') . '%')
+                ->orWhere('kegiatanblud', 'LIKE', '%' . request('q') . '%');
+        })
+        ->get();
+        return new JsonResponse($data);
+    }
+    public function savedata(Request $request)
+    {
+        if
+        (
+            $request->noserahterima === '' || $request->noserahterima === null) {
+            $cek = Serahterima_header::count();
+            $total = (int) $cek + (int) 1;
+            $kodebarang = FormatingHelper::nostp($total, 'SERAHTERIMA');
+        }
+        else
+        {
+            $kodebarang = $request->kodebarang;
+        }
+        $time = date('Y-m-d H:i:s');
+        $user = auth()->user()->pegawai_id;
+        $pg= Pegawai::find($user);
+        $pegawai= $pg->kdpegsimrs;
+        try
+        {
+            DB::beginTransaction();
+            $save = Serahterima_header::updateOrCreate
+            (
+                [
+                    'noserahterimapekerjaan' => $kodebarang
+                ],
+                [
+                    'nokontrak'=>$request->nokontrak ?? '',
+                    'kodepihakketiga'=>$request->kodepihakketiga ?? '',
+                    'namaperusahaan'=>$request->namaperusahaan ?? '',
+                    'kodemapingrs'=>$request->kodemapingrs ?? '',
+                    'namasuplier'=>$request->namasuplier ?? '',
+                    'tglmulaikontrak'=>$request->tglmulaikontrak ?? '',
+                    'tglakhirkontrak'=>$request->tglakhirkontrak ?? '',
+                    'tgltrans'=>$request->tgltrans ?? '',
+                    'kodepptk'=>$request->kodepptk ?? '',
+                    'namapptk'=>$request->namapptk ?? '',
+                    'program'=>$request->program ?? '',
+                    'kegiatan'=>$request->kegiatan ?? '',
+                    'kodekegiatanblud'=>$request->kodekegiatanblud ?? '',
+                    'kegiatanblud'=>$request->kegiatanblud ?? '',
+                    'tglentry'=>$time ?? '',
+                    'userentry'=>$pegawai ?? '',
+                ]
+            );
+            foreach ($request->rincian as $rinci)
+            {
+                $save->rinci()->create(
+                    [
+                        'noserahterimapekerjaan'=>$save->noserahterimapekerjaan,
+                        'nokontrak'=>$rinci['nokontrak'] ?? '',
+                        'koderek50'=>$rinci['koderek50'] ?? '',
+                        'uraianrek50'=>$rinci['uraianrek50'] ?? '',
+                        'koderek108'=>$rinci['koderek108'] ?? '',
+                        'uraian108'=>$rinci['uraian108'] ?? '',
+                        'itembelanja'=>$rinci['itembelanja'] ?? '',
+                        'idserahterima_rinci'=>$rinci['idserahterima_rinci'] ?? '',
+                        'volume'=>$rinci['volume'] ?? '',
+                        'satuan'=>$rinci['satuan'] ?? '',
+                        'harga'=>$rinci['harga'] ?? '',
+                        'total'=>$rinci['total'] ?? '',
+                        'volumels'=>$rinci['volumels'] ?? '',
+                        'hargals'=>$rinci['hargals'] ?? '',
+                        'totalls'=>$rinci['totalls'] ?? '',
+                        'nominalpembayaran'=>$rinci['nominalpembayaran'] ?? '',
+                        'tglentry'=>$time ?? '',
+                        'userentry'=>$pegawai ?? '',
+                    ]
+                );
+            }
+            return new JsonResponse(
+                [
+                    'message' => 'Data Berhasil disimpan...!!',
+                    'result' => $save,
+                ], 200
+            );
+        } catch (\Exception $er) {
+            DB::rollBack();
+            return new JsonResponse(
+                [
+                    'message' => 'Ada Kesalahan',
+                    'error' => $er
+                ], 500
+            );
+        }
+    }
+}
