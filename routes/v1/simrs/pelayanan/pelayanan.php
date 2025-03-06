2044967abf6f1b2476516bf50ca59629f4d0c4e6
duro06 <andi.meka.025@gmail.com>
2025-03-06 09:14:49 +0700

    REV ; duplicate simpan

--- routes/v1/simrs/pelayanan/pelayanan.php
+++ routes/v1/simrs/pelayanan/pelayanan.php
@@ -122,7 +122,8 @@ Route::group([
 
     Route::get('/listresepbynorm', [EresepController::class, 'listresepbynorm']);
     Route::get('/lihatstokobateresepBydokter', [EresepController::class, 'lihatstokobateresepBydokter']);
-    Route::post('/copiresep', [EresepController::class, 'copiresep']);
+    // Route::post('/copiresep', [EresepController::class, 'copiresep']);
+    Route::post('/copiresep', [EresepController::class, 'newCopiResep']);
     Route::get('/listresepbynoreg', [EresepController::class, 'listresepbynoreg']);
 
     Route::post('/kirimpenjaminan', [PoliController::class, 'kirimpenjaminan']);
