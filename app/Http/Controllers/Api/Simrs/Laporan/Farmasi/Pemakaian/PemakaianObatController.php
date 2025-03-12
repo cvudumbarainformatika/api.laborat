4d4300d522845bce9bfb406cdcc98ca234c268a5
duro06 <andi.meka.025@gmail.com>
2025-03-12 09:21:35 +0700

    rev : lap pemakaian obat, bulan lalu akhir

--- app/Http/Controllers/Api/Simrs/Laporan/Farmasi/Pemakaian/PemakaianObatController.php
+++ app/Http/Controllers/Api/Simrs/Laporan/Farmasi/Pemakaian/PemakaianObatController.php
@@ -126,8 +126,8 @@ class PemakaianObatController extends Controller
 
         $dateAwal = Carbon::parse(request('from'));
         $dateAkhir = Carbon::parse(request('to'));
-        $blnLaluAwal = $dateAwal->subMonth()->format('Y-m-d');
-        $blnLaluAkhir = $dateAkhir->subMonth()->format('Y-m');
+        // $blnLaluAwal = $dateAwal->subMonth()->format('Y-m-d');
+        $blnLaluAkhir = $dateAwal->subMonth()->format('Y-m');
         $gudangdepo = ['Gd-03010100', 'Gd-03010101', 'Gd-05010100', 'Gd-04010102', 'Gd-04010103', 'Gd-05010101', 'Gd-02010104']; //
         $obat = Mobatnew::select(
             'kd_obat',
