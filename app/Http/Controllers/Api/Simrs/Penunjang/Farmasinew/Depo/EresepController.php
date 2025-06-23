85eeb5d7b4663c57d3c99837fab6981feced34dd
duro06 <andi.meka.025@gmail.com>
2025-06-23 10:52:18 +0700

    rev : filter noka pada list e-rese[

--- app/Http/Controllers/Api/Simrs/Penunjang/Farmasinew/Depo/EresepController.php
+++ app/Http/Controllers/Api/Simrs/Penunjang/Farmasinew/Depo/EresepController.php
@@ -1335,16 +1335,23 @@ class EresepController extends Controller
         $rm = [];
         if (request('q') !== null) {
             if (preg_match('~[0-9]+~', request('q'))) {
-                $rm = [];
+                $rm = Mpasien::select('rs1')
+                    ->where('rs46', 'LIKE', '%' . request('q') . '%')
+                    ->orWhere('rs1', 'LIKE', '%' . request('q') . '%')
+                    ->pluck('rs1')
+                    ->toArray();
             } else {
                 if (strlen(request('q')) >= 3) {
-                    $data = Mpasien::select('rs1 as norm')->where('rs2', 'LIKE', '%' . request('q') . '%')->get();
-                    $rm = collect($data)->map(function ($x) {
-                        return $x->norm;
-                    })->toArray();
+                    $rm = Mpasien::select('rs1')
+                        ->where('rs2', 'LIKE', '%' . request('q') . '%')
+                        ->pluck('rs1')
+                        ->toArray();
                 } else $rm = [];
             }
         }
+        // return new JsonResponse([
+        //     'rm' => $rm
+        // ]);
         if (request('to') === '' || request('from') === null) {
             $tgl = Carbon::now()->format('Y-m-d 00:00:00');
             $tglx = Carbon::now()->format('Y-m-d 23:59:59');
