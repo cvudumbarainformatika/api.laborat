0c5b2ac608f21d383a849182000d5a672efd7824
pigartpigaro <vichoirul@gmail.com>
2025-07-03 00:25:37 +0700

    wip: rka pergeseran dan perubahan

--- routes/v1/siasik/anggaran/cetakanggaran.php
+++ routes/v1/siasik/anggaran/cetakanggaran.php
@@ -6,10 +6,11 @@ use App\Http\Controllers\Api\Siasik\Anggaran\CetakAnggaranController;
 use Illuminate\Support\Facades\Route;
 
 Route::group([
-    // 'middleware' => 'auth:api',
+    'middleware' => 'auth:api',
     'prefix' => 'siasik/anggaran'
 ], function () {
     Route::get('/getbidangkegiatan', [CetakAnggaranController::class, 'bidangbidangkegiatan']);
     Route::get('/getanggaran', [CetakAnggaranController::class, 'getAnggaran']);
+    Route::get('/getrkapergeseran', [CetakAnggaranController::class, 'getRka']);
 
 });
