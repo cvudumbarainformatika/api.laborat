ef35d43ba3ae45f17a906423a9824d5a61c342ec
kentang <ariezlnd69@gmail.com>
2025-07-08 11:53:04 +0700

    fix : master sistem bayar

--- app/Models/Simrs/Master/Msistembayar.php
+++ app/Models/Simrs/Master/Msistembayar.php
@@ -9,6 +9,11 @@ class Msistembayar extends Model
 {
     use HasFactory;
     protected $table = 'rs9';
+    protected $fillable = ['rs1', 'rs2','rs3','rs4', 'rs5','rs6','rs7','rs8','rs10','pelayanan', 'groups', 'hidden', 'rs9'];
+
+    protected $primaryKey = 'rs1'; // ← ini wajib!
+    public $incrementing = false;  // ← kalau rs1 bukan auto-increment
+    protected $keyType = 'string'; // ← kalau rs1 berupa string,
 
     // protected $guarded=['rs1'];
 }
