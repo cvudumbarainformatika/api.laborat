b7bfbd6ce17b05a555c90272c3b5d436bf56d7db
pigartpigaro <vichoirul@gmail.com>
2025-10-09 09:42:34 +0700

    rev: setting ttd cetak

--- app/Http/Controllers/Api/Siasik/TtdController.php
+++ app/Http/Controllers/Api/Siasik/TtdController.php
@@ -9,16 +9,59 @@ use Illuminate\Http\Request;
 
 class TtdController extends Controller
 {
+    // public function ttdpengesahan()
+    // {
+    //     $pegawai = Mpegawaisimpeg::whereIn('jabatan', ['J00001','J00005','J00034','J00035','J00192'])
+    //     ->where('aktif', 'AKTIF')
+    //     ->select('pegawai.nip',
+    //             'pegawai.nama')
+    //     ->orderBy('pegawai.jabatan', 'asc')
+    //     ->get();
+
+    //     return new JsonResponse($pegawai);
+    // }
+
     public function ttdpengesahan()
     {
-        $pegawai = Mpegawaisimpeg::whereIn('jabatan', ['J00001','J00005','J00034','J00035','J00192'])
+        $direktur = Mpegawaisimpeg::whereIn('jabatan', ['J00001'])
         ->where('aktif', 'AKTIF')
         ->select('pegawai.nip',
                 'pegawai.nama')
-        ->orderBy('pegawai.jabatan', 'asc')
-        ->get();
+        ->first();
 
-        return new JsonResponse($pegawai);
+        $ppk = Mpegawaisimpeg::whereIn('jabatan', ['J00005'])
+        ->where('aktif', 'AKTIF')
+        ->select('pegawai.nip',
+                'pegawai.nama')
+        ->first();
+
+        $bendpenerimaan = Mpegawaisimpeg::whereIn('jabatan', ['J00034'])
+        ->where('aktif', 'AKTIF')
+        ->select('pegawai.nip',
+                'pegawai.nama')
+        ->first();
+
+        $bendpengeluaran = Mpegawaisimpeg::whereIn('jabatan', ['J00035'])
+        ->where('aktif', 'AKTIF')
+        ->select('pegawai.nip',
+                'pegawai.nama')
+        ->first();
+
+        $verifikator = Mpegawaisimpeg::whereIn('jabatan', ['J00192'])
+        ->where('aktif', 'AKTIF')
+        ->select('pegawai.nip',
+                'pegawai.nama')
+        ->first();
+
+        $data = [
+            'direktur' => $direktur,
+            'ppk' => $ppk,
+            'bendpenerimaan' => $bendpenerimaan,
+            'bendpengeluaran' => $bendpengeluaran,
+            'verifikator' => $verifikator,
+        ];
+
+        return new JsonResponse($data);
     }
 
 }
