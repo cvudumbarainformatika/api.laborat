737dea1535d7dd6e2069e5b5e71f94315149a478
duro06 <andi.meka.025@gmail.com>
2025-12-22 14:19:32 +0700

    rev : persiapan operasi

--- app/Http/Controllers/Api/Simrs/Master/TindakanController.php
+++ app/Http/Controllers/Api/Simrs/Master/TindakanController.php
@@ -106,4 +106,59 @@ class TindakanController extends Controller
         $caritindakan->save();
         return new JsonResponse(['message' => 'ok'], 200);
     }
+    public function simpanTindakanKeTabelSementara(Request $request)
+    {
+
+        if ($request->kdtindakan == '' || $request->kdtindakan == null) {
+            $ceknama = Mtindakan::where('rs2', $request->nmtindakan)->count();
+            if ($ceknama > 0) {
+                return new JsonResponse(['message' => 'Maaf Tindakan Sudah Ada...!!!'], 500);
+            }
+
+            $cektotal = Mtindakan::count();
+            $akhir = (int) $cektotal + (int) 1;
+
+            $has = null;
+            $lbr = strlen($akhir);
+            for ($i = 1; $i <= 4 - $lbr; $i++) {
+                $has = $has . "0";
+            }
+
+            $kdtindakan = 'TB' . $has . $akhir;
+        } else {
+            $kdtindakan = $request->kdtindakan;
+        }
+        $simpantindakan = Mtindakan::updateOrCreate(
+            [
+                'rs1' => $kdtindakan
+            ],
+            [
+                'rs2' => $request->nmtindakan,
+                'rs3' => 'T1#',
+                'rs8' => $request->js3,
+                'rs9' => $request->jp3,
+                'rs10' => $request->habispake3,
+                'rs11' => $request->js2,
+                'rs12' => $request->jp2,
+                'rs13' => $request->habispake2,
+                'rs14' => $request->js1,
+                'rs15' => $request->jp1,
+                'rs16' => $request->habispake1,
+                'rs17' => $request->jsutama,
+                'rs18' => $request->jputama,
+                'rs19' => $request->habispakeutama,
+                'rs20' => $request->jsvip,
+                'rs21' => $request->jpvip,
+                'rs22' => $request->habispakevip,
+                'rs23' => $request->jsvvip,
+                'rs24' => $request->jpvvip,
+                'rs25' => $request->habispakevvip
+            ]
+        );
+        if (!$simpantindakan) {
+            return new JsonResponse(['message' => 'Data Gagal Disimpan...!!!'], 410);
+        }
+        return new JsonResponse($simpantindakan);
+    }
+    public function pindahKeTabelMaster() {}
 }
