036dbed6ff9f998f8fe6d9e69c3e19df223ab0b7
pigartpigaro <vichoirul@gmail.com>
2026-02-24 10:41:13 +0700

    rev: penyesuaian prioritas

---
+++ app/Models/Siasik/Anggaran/Tampung_Pagu.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Siasik\Anggaran;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tampung_Pagu extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/Siasik/Anggaran/Tampung_Pagu_Copy.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Siasik\Anggaran;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tampung_Pagu_Copy extends Model
+{
+    use HasFactory;
+}
