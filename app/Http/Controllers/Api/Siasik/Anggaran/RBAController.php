1852a76025fb9a067fbfd838b757152a7cd9e274
pigartpigaro <vichoirul@gmail.com>
2026-04-10 09:07:48 +0700

    rev: RBA

--- app/Http/Controllers/Api/Siasik/Anggaran/RBAController.php
+++ app/Http/Controllers/Api/Siasik/Anggaran/RBAController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\Siasik\Anggaran;
 
 use App\Http\Controllers\Controller;
+use App\Models\Siasik\Anggaran\Penyesuaian_Prioritas_Header;
 use App\Models\Siasik\Anggaran\PergeseranPaguRinci;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
@@ -24,12 +25,16 @@ class RBAController extends Controller
             't_tampung.volume',
             't_tampung.harga',
             't_tampung.satuan',
+            't_tampung.idpp',
             'akun50_2024.kodeall3 as kode',
             'akun50_2024.uraian as uraian',
             'mappingpptkkegiatan.kodekegiatan',
             'mappingpptkkegiatan.kegiatan',
             'mappingpptkkegiatan.kodebidang',
             'mappingpptkkegiatan.bidang',
+            DB::raw('0 as paguawal'),
+            DB::raw('0 as hargaawal'),
+            DB::raw('0 as volumeawal'),
             DB::raw('SUBSTRING_INDEX(akun50_2024.kodeall3, ".", 1) as kode1'),
             DB::raw('SUBSTRING_INDEX(akun50_2024.kodeall3, ".", 2) as kode2'),
             DB::raw('SUBSTRING_INDEX(akun50_2024.kodeall3, ".", 3) as kode3'),
@@ -45,6 +50,44 @@ class RBAController extends Controller
         ->orderBy('kode', 'asc')
         ->get();
 
-        return new JsonResponse($anggaran);
+        $anggaranawal = Penyesuaian_Prioritas_Header::whereBetween('penyesesuaianperioritas_heder.tgltrans', [$thn.'-01-01', $thn.'-12-31'])
+            ->leftJoin('penyesesuaianperioritas_rinci', 'penyesesuaianperioritas_rinci.notrans', '=', 'penyesesuaianperioritas_heder.notrans')
+            ->leftJoin('akun50_2024', 'akun50_2024.kodeall2', '=', 'penyesesuaianperioritas_rinci.koderek50')
+            ->select(
+                'penyesesuaianperioritas_heder.notrans',
+                'penyesesuaianperioritas_heder.namabidang as bidang',
+                'penyesesuaianperioritas_heder.kodebidang',
+                'penyesesuaianperioritas_heder.kodekegiatan',
+                'penyesesuaianperioritas_rinci.id as idpp',
+                'penyesesuaianperioritas_rinci.usulan',
+                'penyesesuaianperioritas_rinci.koderek108',
+                'penyesesuaianperioritas_rinci.koderek50',
+                'penyesesuaianperioritas_rinci.uraian108',
+                'penyesesuaianperioritas_rinci.jumlahacc as volumeawal',
+                'penyesesuaianperioritas_rinci.harga as hargaawal',
+                'penyesesuaianperioritas_rinci.nilai as paguawal',
+                'penyesesuaianperioritas_rinci.satuan as satuanawal',
+                'akun50_2024.kodeall3 as kode',
+                'akun50_2024.uraian as uraian',
+                DB::raw('0 as pagu'),
+                DB::raw('0 as harga'),
+                DB::raw('0 as volume'),
+                DB::raw('SUBSTRING_INDEX(akun50_2024.kodeall3, ".", 1) as kode1'),
+                DB::raw('SUBSTRING_INDEX(akun50_2024.kodeall3, ".", 2) as kode2'),
+                DB::raw('SUBSTRING_INDEX(akun50_2024.kodeall3, ".", 3) as kode3'),
+                DB::raw('SUBSTRING_INDEX(akun50_2024.kodeall3, ".", 4) as kode4'),
+                DB::raw('SUBSTRING_INDEX(akun50_2024.kodeall3, ".", 5) as kode5'),
+                DB::raw('(SELECT uraian FROM akun50_2024 WHERE kodeall2 = SUBSTRING_INDEX(penyesesuaianperioritas_rinci.koderek50, ".", 1) LIMIT 1) as uraian1'),
+                DB::raw('(SELECT uraian FROM akun50_2024 WHERE kodeall2 = SUBSTRING_INDEX(penyesesuaianperioritas_rinci.koderek50, ".", 2) LIMIT 1) as uraian2'),
+                DB::raw('(SELECT uraian FROM akun50_2024 WHERE kodeall2 = SUBSTRING_INDEX(penyesesuaianperioritas_rinci.koderek50, ".", 3) LIMIT 1) as uraian3'),
+                DB::raw('(SELECT uraian FROM akun50_2024 WHERE kodeall2 = SUBSTRING_INDEX(penyesesuaianperioritas_rinci.koderek50, ".", 4) LIMIT 1) as uraian4'),
+                DB::raw('(SELECT uraian FROM akun50_2024 WHERE kodeall2 = SUBSTRING_INDEX(penyesesuaianperioritas_rinci.koderek50, ".", 5) LIMIT 1) as uraian5'),
+            )
+            
+            ->get();
+        return new JsonResponse([
+            'anggaran' => $anggaran,
+            'anggaranawal' => $anggaranawal
+        ]);
     }
 }
