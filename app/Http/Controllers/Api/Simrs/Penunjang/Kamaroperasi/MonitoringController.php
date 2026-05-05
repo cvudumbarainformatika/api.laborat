afdefa3201f7068ea8bd4d66571e4b1b41ef0975
duro06 <andi.meka.025@gmail.com>
2026-05-05 10:10:02 +0700

    feat : monitoring pasca

--- app/Http/Controllers/Api/Simrs/Penunjang/Kamaroperasi/MonitoringController.php
+++ app/Http/Controllers/Api/Simrs/Penunjang/Kamaroperasi/MonitoringController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api\Simrs\Penunjang\Kamaroperasi;
 
 use App\Http\Controllers\Controller;
+use App\Models\Simrs\Penunjang\Kamaroperasi\LogMonitoringPascaAnastesi;
 use App\Models\Simrs\Penunjang\Kamaroperasi\LogMonitoringSelamaAnastesi;
+use App\Models\Simrs\Penunjang\Kamaroperasi\MedikasiPascaAnastesi;
 use App\Models\Simrs\Penunjang\Kamaroperasi\MedikasiSelamaAnastesi;
 use Exception;
 use Illuminate\Http\JsonResponse;
@@ -95,6 +97,92 @@ class MonitoringController extends Controller
                 'trace' => $e->getTrace(),
 
             ], 410);
+        }
+    }
+
+    /** 
+     *  pasca anastesi
+     */
+    public function getLogPasca()
+    {
+        // Mengambil data log berdasarkan noreg, nota, dan norm
+        $data['monitoring'] = LogMonitoringPascaAnastesi::where('noreg', request('noreg'))
+            ->where('nota', request('nota'))
+            ->where('norm', request('norm'))
+            ->orderBy('time', 'asc')
+            ->get();
+        $data['medikasi'] = MedikasiPascaAnastesi::where('noreg', request('noreg'))
+            ->where('nota', request('nota'))
+            ->where('norm', request('norm'))
+            ->first();
+
+        return new JsonResponse($data);
+    }
+
+    public function simpanLogPasca(Request $request)
+    {
+        $request->validate([
+            'noreg' => 'required',
+            'nota' => 'required',
+            'norm' => 'required',
+            'time' => 'required', // Menit keberapa di RR
+        ]);
+
+        try {
+            DB::beginTransaction();
+
+            // Menggunakan updateOrCreate agar jika user mengedit menit yang sama, data tertimpa
+            $data = LogMonitoringPascaAnastesi::updateOrCreate(
+                [
+                    'noreg' => $request->noreg,
+                    'nota' => $request->nota,
+                    'norm' => $request->norm,
+                    'time' => $request->time,
+                ],
+                $request->all()
+            );
+
+            if (!$data) throw new \Exception('Gagal menyimpan log pasca anestesi');
+
+            DB::commit();
+            return new JsonResponse([
+                'data' => $data,
+                'message' => 'Log monitoring berhasil disimpan'
+            ]);
+        } catch (\Throwable $e) {
+            DB::rollBack();
+            return new JsonResponse(['message' => $e->getMessage()], 410);
+        }
+    }
+    public function simpanMedikasiPasca(Request $request)
+    {
+        $request->validate([
+            'noreg' => 'required',
+            'norm' => 'required',
+            'nota' => 'required',
+        ]);
+
+        try {
+            DB::beginTransaction();
+            $data = MedikasiPascaAnastesi::updateOrCreate(
+                [
+                    'noreg' => $request->noreg,
+                    'nota' => $request->nota,
+                    'norm' => $request->norm,
+                ],
+                $request->all()
+            );
+
+            if (!$data) throw new Exception('Gagal menyimpan data medikasi pasca');
+
+            DB::commit();
+            return new JsonResponse([
+                'data' => $data,
+                'message' => 'Instruksi Pasca Anestesi berhasil disimpan'
+            ]);
+        } catch (\Throwable $e) {
+            DB::rollBack();
+            return new JsonResponse(['message' => $e->getMessage()], 410);
         }
     }
 }
