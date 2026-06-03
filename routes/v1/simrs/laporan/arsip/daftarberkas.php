6b7361ae06d933852f48f69bd3e5e9cceae4297a
kentangkotak <ariezlnd69@gmail.com>
2026-06-03 14:47:01 +0700

    FIX : LAPORAN ARSIP

---
+++ routes/v1/simrs/laporan/arsip/daftarberkas.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\Api\Simrs\Laporan\Arsip\daftarberkasController;
+use Illuminate\Support\Facades\Route;
+
+Route::group([
+    'middleware' => 'auth:api',
+    // 'middleware' => 'jwt.verify',
+    'prefix' => 'simrs/laporan/arsip/daftarberkas'
+], function () {
+    Route::get('/get-data', [daftarberkasController::class, 'getData']);
+});
