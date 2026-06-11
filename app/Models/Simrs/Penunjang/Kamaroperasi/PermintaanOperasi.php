2de9e843c245500fd4eb8720824392e4718fdf48
duro06 <andi.meka.025@gmail.com>
2026-06-11 17:30:06 +0700

    rev : resolve luput

--- app/Models/Simrs/Penunjang/Kamaroperasi/PermintaanOperasi.php
+++ app/Models/Simrs/Penunjang/Kamaroperasi/PermintaanOperasi.php
@@ -108,4 +108,26 @@ class PermintaanOperasi extends Model
     {
         return $this->hasMany(Tindakan::class, 'rs1', 'rs1');
     }
+    public function laborats()
+    {
+        return $this->hasMany(LaboratMeta::class, 'noreg', 'rs1');
+    }
+    public function laboratold()
+    {
+        return $this->hasMany(Laboratpemeriksaan::class, 'rs1', 'rs1');
+    }
+
+    public function bankdarah()
+    {
+        return $this->hasMany(PermintaanBankdarah::class, 'rs1', 'rs1');
+    }
+
+    public function radiologi()
+    {
+        return $this->hasMany(Transpermintaanradiologi::class, 'rs1', 'rs1');
+    }
+    public function hasilradiologi()
+    {
+        return $this->hasMany(PembacaanradiologiController::class, 'rs1', 'rs1');
+    }
 }
