bd63bc5457ac65caeceff150817aa1903bd2b7d4
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2026-06-17 13:55:15 +0700

    feta: logs activity

--- app/Models/Simrs/Ranap/Pelayanan/NotasiDpjp.php
+++ app/Models/Simrs/Ranap/Pelayanan/NotasiDpjp.php
@@ -3,12 +3,13 @@
 namespace App\Models\Simrs\Ranap\Pelayanan;
 
 use App\Models\Simpeg\Petugas;
+use App\Traits\LogsActivity;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class NotasiDpjp extends Model
 {
-  use HasFactory;
+  use HasFactory, LogsActivity;
   protected $connection = 'mysql';
   protected $table = 'cppt_notasi';
   protected $guarded = ['id'];
--- app/Models/Simrs/Ranap/Pelayanan/Pemeriksaan/PemeriksaanSambung.php
+++ app/Models/Simrs/Ranap/Pelayanan/Pemeriksaan/PemeriksaanSambung.php
@@ -4,12 +4,13 @@ namespace App\Models\Simrs\Ranap\Pelayanan\Pemeriksaan;
 
 use App\Models\Pegawai\Mpegawaisimpeg;
 use App\Models\Simpeg\Petugas;
+use App\Traits\LogsActivity;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PemeriksaanSambung extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivity;
     protected $connection = 'mysql';
     protected $table = 'rs253_sambung';
     protected $guarded = ['id'];
