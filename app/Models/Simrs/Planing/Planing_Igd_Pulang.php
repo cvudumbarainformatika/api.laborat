94564bed32b1d19e1aa3e376eb11f4662c154dcc
pigartpigaro <vichoirul@gmail.com>
2026-07-09 11:20:04 +0700

    rev: tanbah ttd

--- app/Models/Simrs/Planing/Planing_Igd_Pulang.php
+++ app/Models/Simrs/Planing/Planing_Igd_Pulang.php
@@ -5,6 +5,8 @@ namespace App\Models\Simrs\Planing;
 use App\Models\Simpeg\Petugas;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\URL;
+use Intervention\Image\ImageManager;
 
 class Planing_Igd_Pulang extends Model
 {
@@ -15,5 +17,27 @@ class Planing_Igd_Pulang extends Model
     public function dokterpenangungjawabpulang()
     {
         return $this->hasOne(Petugas::class, 'kdpegsimrs', 'user_dokter');
+    }
+
+    public function getTtdYgMenyatakanUrlAttribute()
+    {
+        $image = URL::to('/storage/' . $this->ttdYgMenyatakan);
+        if (!$image) {
+            return null;
+        }
+        $handle = @fopen($image, 'r');
+        if ($handle) {
+            // $base64 = 'data:image/png;base64,' . base64_encode(file_get_contents($image));
+            $manager = new ImageManager();
+            $base64 = (string) $manager->make($image)->resize(100, null, function ($constraint) {
+                $constraint->aspectRatio();
+            })->encode('data-url');
+
+            $result =  $base64 ? $base64 : null;
+            // return $this->ttdDokter ? $base64 : null;
+            return $result;
+        } else {
+            return null;
+        }
     }
 }
