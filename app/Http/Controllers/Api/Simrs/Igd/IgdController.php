2597bff5922d31887b11fe834ce45b810c675d7f
kentangkotak <ariezlnd69@gmail.com>
2026-07-20 09:32:53 +0700

    FIX : INDIKASI MASUK RAWAT INAP

--- app/Http/Controllers/Api/Simrs/Igd/IgdController.php
+++ app/Http/Controllers/Api/Simrs/Igd/IgdController.php
@@ -480,6 +480,11 @@ class IgdController extends Controller
             if ($cekplan === '1') {
                 return new JsonResponse(['message' => 'Maaf Form Planing Belum Diisi...!!!'], 500);
             }
+
+            $cekberkas = CekController::cekindikasimasukranap($request->noreg);
+            if($cekberkas !== 1){
+                return new JsonResponse(['message' => 'Maaf Form Planing (Indikasi Pasien Masuk Rawat Inap)...!!!'], 500);
+            }
         }
 
 
--- app/Http/Controllers/Api/Simrs/Syarat/CekController.php
+++ app/Http/Controllers/Api/Simrs/Syarat/CekController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Simrs\Syarat;
 use App\Http\Controllers\Controller;
 use App\Models\Simrs\Master\Mpasien;
 use App\Models\Simrs\Planing\Planing_Igd_Lama;
+use App\Models\Simrs\Planing\Plann_Igd_Ranap_Ruang;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -28,6 +29,27 @@ class CekController extends Controller
             return "1";
         }else{
             return "2";
+        }
+    }
+
+    public static function cekindikasimasukranap($noreg)
+    {
+        $cari = Planing_Igd_Lama::query()
+            ->join('rs24', 'rs24.rs1', '=', 'rs141.rs5')
+            ->where('rs141.rs1', $noreg)
+            ->where('rs141.rs3', 'POL014')
+            ->where('rs141.rs4', 'Rawat Inap')
+            ->select('rs141.rs1 as noreg','rs141.rs2 as norm','rs141.rs5 as ruangan', 'rs24.3 as kelas')
+            ->first();
+        if($cari->kelas === 'NICU' || $cari->kelas === 'ICC' || $cari->kelas === 'IC' || $cari->kelas === 'HCU'){
+            $cek = Plann_Igd_Ranap_Ruang::where('noreg', $noreg)->count();
+            if($cek > 0){
+                return 1;
+            }else{
+                return 0;
+            }
+        }else{
+            return 1;
         }
     }
 }
